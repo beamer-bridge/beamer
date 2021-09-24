cef4e6aa976ff269779efb06b328b464f0dfe3b9
Frederik Lührs <luehrs.fred@gmail.com>
2021-09-24 16:06:07 +0200

    first tests

--- contracts/tests/test_claim_manager.py
+++ contracts/tests/test_claim_manager.py
@@ -1,4 +1,12 @@
+from brownie import accounts
+import brownie
 
-def test_deploy_claim_manager():
-    ClaimManager.deploy()
+def test_claim_with_different_values(claim_manager, claim_stake):
 
+    claim_manager.claimRequest(123, {"from": accounts[0], "value": claim_stake})
+
+    with brownie.reverts("Stake provided not correct"):
+        claim_manager.claimRequest(123, {"from": accounts[0], "value": claim_stake-1})
+
+    with brownie.reverts("Stake provided not correct"):
+        claim_manager.claimRequest(123, {"from": accounts[0], "value": claim_stake+1})
