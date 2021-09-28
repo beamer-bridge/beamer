dde2c6303cab3ef6bd2fb8cb5625b2c3b6651374
Frederik Lührs <luehrs.fred@gmail.com>
2021-09-28 15:55:45 +0200

    add claim stake fixture to contract deployment

--- contracts/tests/conftest.py
+++ contracts/tests/conftest.py
@@ -2,10 +2,12 @@ import pytest
 
 from brownie import accounts
 
+
 @pytest.fixture
 def claim_stake():
     return 100
 
+
 @pytest.fixture
 def claim_manager(ClaimManager, claim_stake):
-    return ClaimManager.deploy(100, 100, 100, 100, {"from": accounts[0]})
+    return ClaimManager.deploy(claim_stake, 100, 100, 100, {"from": accounts[0]})
