b783f0bac4279b7ebdbb71bf3a4e45df6d498e9f
Paul Lange <palango@gmx.de>
2021-10-11 16:37:28 +0200

    Add basic tests for `FillManager`

---
+++ contracts/test.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "OpenZeppelin/openzeppelin-contracts@4.3.2/contracts/token/ERC20/ERC20.sol";
+
+
+contract TestToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Test", "TST") {
+        _mint(msg.sender, initialSupply);
+    }
+}
