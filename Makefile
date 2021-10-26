3799eb20bfdf3de66e39591947e73ba0bf9cc814
Paul Lange <palango@gmx.de>
2021-10-26 16:35:03 +0200

    chore: Add Makefile and format python files

---
+++ Makefile
@@ -0,0 +1,15 @@
+CODE_DIRS = contracts/ raisync/
+ISORT_PARAMS = --ignore-whitespace $(CODE_DIRS)
+
+all: lint
+
+lint:
+	black --check --diff $(CODE_DIRS)
+	flake8 $(CODE_DIRS)
+	isort $(ISORT_PARAMS) --diff --check-only
+	pylint $(CODE_DIRS)
+
+black:
+	black $(CODE_DIRS)
+
+format: black
--- docker/run.sh
+++ docker/run.sh
@@ -8,12 +8,13 @@ function finish() {
   docker-compose down || true
 }
 
+trap finish EXIT
 
 echo -e "\nStarting the end-to-end environment"
 docker-compose -f docker-compose-nobuild.yml up -d
 
 echo -e "\nWait to make sure all services are up and running"
-sleep 10s
+sleep 10
 
 # It'ds currently not possible to define networks in a per-project way
 # Instead, update the config
@@ -21,8 +22,6 @@ poetry run brownie networks add Ethereum l1 host="http://0.0.0.0:9545" chainid=1
 poetry run brownie networks add Ethereum l2 host="http://0.0.0.0:8545" chainid=420 || true
 
 # Deploy contracts
-cd contracts/ && poetry run brownie run deploy.py --network l2
+cd ../contracts/ && poetry run brownie run deploy.py --network l2
 
 # Run tests
-
-trap finish EXIT
