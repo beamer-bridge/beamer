9a4a53a2a03a420d9fc93635a393fe6605593bc2
Paul Lange <palango@gmx.de>
2021-10-27 12:13:54 +0200

    e2e: Deploy using prefunded account

--- contracts/scripts/deploy.py
+++ contracts/scripts/deploy.py
@@ -2,9 +2,12 @@ from brownie import DummyProofSubmitter, FillManager, RequestManager, accounts
 
 
 def main():
+    accounts.add("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80")
+    deployer = accounts.at("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266")
+
     l1_resolver_address = "0x0000000000000000000000000000000000000001"
 
-    proof_submitter = DummyProofSubmitter.deploy({"from": accounts[0]})
-    RequestManager.deploy({"from": accounts[0]})
+    proof_submitter = DummyProofSubmitter.deploy({"from": deployer})
+    RequestManager.deploy({"from": deployer})
 
-    FillManager.deploy(l1_resolver_address, proof_submitter.address, {"from": accounts[0]})
+    FillManager.deploy(l1_resolver_address, proof_submitter.address, {"from": deployer})
--- docker/run.sh
+++ docker/run.sh
@@ -14,7 +14,7 @@ echo -e "\nStarting the end-to-end environment"
 docker-compose -f docker-compose-nobuild.yml up -d
 
 echo -e "\nWait to make sure all services are up and running"
-sleep 10
+sleep 5
 
 # It's currently not possible to define networks in a per-project way
 # Instead, update the config
@@ -22,6 +22,6 @@ poetry run brownie networks add Ethereum l1 host="http://0.0.0.0:9545" chainid=1
 poetry run brownie networks add Ethereum l2 host="http://0.0.0.0:8545" chainid=420 || true
 
 # Deploy contracts
-cd ../contracts/ && poetry run brownie run deploy.py --network l2
-
+SCRIPT_DIR=`dirname "$(realpath "$0")"`
+cd "${SCRIPT_DIR}/../contracts" && poetry run brownie run deploy.py --network l2 && cd "${SCRIPT_DIR}"
 # Run tests
