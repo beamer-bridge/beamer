189e943eaf22b802888c80a2ee64ec08215b5edd
Paul Lange <palango@gmx.de>
2021-10-27 11:49:03 +0200

    e2e: Cleanup script

--- docker/run.sh
+++ docker/run.sh
@@ -5,7 +5,7 @@ function finish() {
 #   echo -e "\nGet the log files of the run services"
 #   docker cp "$DOCKER_CONTAINER_NAME":/var/log/supervisor/. ./logs/ || true
   echo -e "\nShutting down the end-to-end environment"
-  docker-compose down || true
+  docker-compose -f docker-compose-nobuild.yml down || true
 }
 
 trap finish EXIT
@@ -16,7 +16,7 @@ docker-compose -f docker-compose-nobuild.yml up -d
 echo -e "\nWait to make sure all services are up and running"
 sleep 10
 
-# It'ds currently not possible to define networks in a per-project way
+# It's currently not possible to define networks in a per-project way
 # Instead, update the config
 poetry run brownie networks add Ethereum l1 host="http://0.0.0.0:9545" chainid=1337 || true
 poetry run brownie networks add Ethereum l2 host="http://0.0.0.0:8545" chainid=420 || true
