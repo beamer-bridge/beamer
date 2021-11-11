b349bd3c44c1974aded4132bc5dd2fd109b1167b
Ivan Stanković <ivan@brainbot.com>
2021-11-11 12:09:17 +0100

    contracts: add a standard ERC20 token contract
    The json file is going to be used by the raisync node to get the
    ERC20 contract ABI.

---
+++ contracts/contracts/StandardToken.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "OpenZeppelin/openzeppelin-contracts@4.3.2/contracts/token/ERC20/ERC20.sol";
+
+abstract contract StandardToken is ERC20 {
+}
