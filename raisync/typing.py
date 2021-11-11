0d5e2e1f6fbcb832eb71429f55d5dc121d3c3d33
Ivan Stanković <ivan@brainbot.com>
2021-11-11 15:32:24 +0100

    raisync: typing: add RequestId and URL

--- raisync/typing.py
+++ raisync/typing.py
@@ -8,5 +8,7 @@ from eth_typing import (  # NOQA pylint:disable=unused-import
 )
 
 ChainId = NewType("ChainId", int)
+RequestId = NewType("RequestId", int)
 PrivateKey = NewType("PrivateKey", bytes)
 TokenAmount = NewType("TokenAmount", int)
+URL = NewType("URL", str)
