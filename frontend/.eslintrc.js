5f5a829c88bd1d2da5b83231711abbb0a77cb2c4
manuelwedler <manuel.wedler@googlemail.com>
2021-11-11 14:18:49 +0100

    Init frontend prototype project

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  plugins: ['simple-import-sort', 'import'],
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:vue/vue3-strongly-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'sort-imports': 'off',
+    'import/order': 'off',
+    'import/export': 'off',
+    'import/first': 'warn',
+    'import/newline-after-import': 'warn',
+    'import/no-extraneous-dependencies': 'warn',
+    'import/no-duplicates': 'warn',
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
---
+++ frontend/src/main.ts
@@ -0,0 +1,8 @@
+import './main.scss';
+
+import { createApp } from 'vue';
+
+import App from './App.vue';
+import router from './router';
+
+createApp(App).use(router).mount('#app');
