fda7f6ccd434be870763f1784e2396707c7a8315
Ivan Stanković <ivan@brainbot.com>
2021-11-15 15:14:34 +0100

    contracts: add missing types

--- contracts/scripts/deploy.py
+++ contracts/scripts/deploy.py
@@ -1,7 +1,7 @@
 from brownie import DummyProofSubmitter, FillManager, MintableToken, RequestManager, Wei, accounts
 
 
-def main():
+def main() -> None:
     accounts.add("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80")
     deployer = accounts.at("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266")
 
