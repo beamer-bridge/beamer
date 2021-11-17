2a15f8cf39726eae25fea2e85107630bbb08e53f
Ivan Stanković <ivan@brainbot.com>
2021-11-17 15:55:28 +0100

    docker: improve the optimism setup script

--- docker/run.sh
+++ docker/run.sh
@@ -1,27 +1,55 @@
 #!/usr/bin/env bash
 
-function finish() {
-# Implement logs download
-#   echo -e "\nGet the log files of the run services"
-#   docker cp "$DOCKER_CONTAINER_NAME":/var/log/supervisor/. ./logs/ || true
-  echo -e "\nShutting down the end-to-end environment"
-  docker-compose -f docker-compose-nobuild.yml down || true
+ROOT=$(dirname $(dirname "$(realpath "$0")"))
+DOCKER_COMPOSE_FILE="${ROOT}/docker/docker-compose-nobuild.yml"
+
+function down {
+    echo -e "\nShutting down the end-to-end environment"
+    docker-compose -f "${DOCKER_COMPOSE_FILE}" down
 }
 
-trap finish EXIT
+function up {
+    echo -e "\nStarting the end-to-end environment"
+    docker-compose -f "${DOCKER_COMPOSE_FILE}" up -d
 
-echo -e "\nStarting the end-to-end environment"
-docker-compose -f docker-compose-nobuild.yml up -d
+    echo -e "\nWait to make sure all services are up and running"
+    sleep 40
 
-echo -e "\nWait to make sure all services are up and running"
-sleep 5
+    # It's currently not possible to define networks in a per-project way
+    # Instead, update the config
+    poetry run brownie networks list | grep -q l1 || \
+        poetry run brownie networks add Ethereum l1 host="http://0.0.0.0:9545" chainid=1337
+    poetry run brownie networks list | grep -q l2 || \
+        poetry run brownie networks add Ethereum l2 host="http://0.0.0.0:8545" chainid=420
 
-# It's currently not possible to define networks in a per-project way
-# Instead, update the config
-poetry run brownie networks add Ethereum l1 host="http://0.0.0.0:9545" chainid=1337 || true
-poetry run brownie networks add Ethereum l2 host="http://0.0.0.0:8545" chainid=420 || true
+    rm -rf "${ROOT}/contracts/build/deployments/"
+    # Deploy contracts
+    pushd "${ROOT}/contracts"
+    poetry run brownie run deploy.py --network l2
+    popd
+}
 
-# Deploy contracts
-SCRIPT_DIR=`dirname "$(realpath "$0")"`
-cd "${SCRIPT_DIR}/../contracts" && poetry run brownie run deploy.py --network l2 && cd "${SCRIPT_DIR}"
-# Run tests
+function usage() {
+    cat <<EOF
+$0  [up | down]
+
+Commands:
+  up        Bring up a private Optimism instance. Deploy Raisync contracts on it.
+  down      Stop the Optimism instance.
+EOF
+}
+
+case $1 in
+    up)
+        up
+        ;;
+
+    down)
+        down
+        ;;
+
+    *)
+        usage
+        exit 1
+        ;;
+esac
