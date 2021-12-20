cee2997d54871acf444795eb9a88fd120e6c8a2f
Ivan Stanković <ivan@brainbot.com>
2021-12-20 11:09:56 +0100

    raisync: tests: add a CLI test
    This brings the coverage to 92%.

---
+++ raisync/tests/test_cli.py
@@ -0,0 +1,42 @@
+import json
+import pathlib
+import signal
+
+import eth_account
+
+from click.testing import CliRunner
+from raisync.cli import main
+
+
+def test_cli(config, tmp_path):
+    key = "0x3ff6c8dfd3ab60a14f2a2d4650387f71fe736b519d990073e650092faaa621fa"
+    acc = eth_account.Account.from_key(key)
+    obj = eth_account.account.create_keyfile_json(acc.key, b"")
+    keyfile = tmp_path / f"{acc.address}.json"
+    keyfile.write_text(json.dumps(obj))
+    contracts_deployment_dir = str(
+        pathlib.Path(__file__).parents[2] / "contracts/build/deployments/dev"
+    )
+
+    signal.signal(signal.SIGALRM, lambda *_unused: signal.raise_signal(signal.SIGINT))
+    signal.setitimer(signal.ITIMER_REAL, 2)
+
+    runner = CliRunner()
+    result = runner.invoke(
+        main,
+        [
+            "--keystore-file",
+            str(keyfile),
+            "--password",
+            "",
+            "--l2a-rpc-url",
+            config.l2a_rpc_url,
+            "--l2b-rpc-url",
+            config.l2b_rpc_url,
+            "--l2a-contracts-deployment-dir",
+            contracts_deployment_dir,
+            "--l2b-contracts-deployment-dir",
+            contracts_deployment_dir,
+        ],
+    )
+    assert result.exit_code == 0
