c7f73f2b443eee26d0a9b8fef6f9ce2c541b89b2
Paul Lange <palango@gmx.de>
2022-01-12 17:43:51 +0100

    tests: Adapt tests to fees
    We set the fees to zero for the normal tests

--- contracts/tests/test_request_manager_fees.py
+++ contracts/tests/test_request_manager_fees.py
@@ -1,25 +1,17 @@
 from brownie import accounts
 
+from contracts.tests.utils import make_request
+
 
 def test_only_sender_can_cancel_request(request_manager, token):
     requester = accounts[1]
     transfer_amount = 23
 
-    f1 = request_manager.gasReimbursementFee()
-    f2 = request_manager.lpServiceFee()
-    f3 = request_manager.raisyncServiceFee()
+    request_id = make_request(request_manager, token, requester, transfer_amount)
 
-    token.mint(requester, transfer_amount, {"from": requester})
-    token.approve(request_manager.address, transfer_amount, {"from": requester})
-    request_tx = request_manager.createRequest(
-        1,
-        token.address,
-        token.address,
-        "0x5d5640575161450A674a094730365A223B226649",
-        transfer_amount,
-        {"from": requester, "value": f1 + f2 + f3},
-    )
-    request_id = request_tx.return_value
+    reimbursement_fee = request_manager.gasReimbursementFee()
+    lp_service_fee = request_manager.lpServiceFee()
+    raisync_fee = request_manager.raisyncServiceFee()
 
-    assert request_manager.collectedRaisyncFees() == 62856900000000
-    assert request_manager.requests(request_id)[9] == 1459676900000000
+    assert request_manager.collectedRaisyncFees() == raisync_fee
+    assert request_manager.requests(request_id)[9] == reimbursement_fee + lp_service_fee
