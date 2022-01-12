264dc83808f5c1be0d704db43cc47e8979bc8d62
Ivan Stanković <ivan@brainbot.com>
2022-01-12 18:06:25 +0100

    contracts: merge challenges into claims
    This simplifies the code somewhat. Effects on gas:
    
    DummyProofSubmitter
       └─ constructor    - avg:      94,887       94,887            0
    FillManager
       ├─ constructor    - avg:     488,881      488,881            0
       ├─ fillRequest    - avg:      67,105       67,105            0
       └─ addAllowedLP   - avg:      43,460       43,460            0
    MintableToken
       ├─ constructor    - avg:     693,116      693,116            0
       ├─ mint           - avg:      45,688       45,688            0
       └─ approve        - avg:      44,085       44,085            0
    RequestManager
       ├─ constructor    - avg:   1,907,631    1,726,124     -181,507
       ├─ createRequest  - avg:     189,810      189,810            0
       ├─ claimRequest   - avg:     126,402      154,634      +28,232
       ├─ challengeClaim - avg:     108,822       84,578      -24,244
       ├─ withdraw       - avg:      49,004       64,081      +15,077
       └─ updateFeeData  - avg:      16,243       16,254          +11

--- contracts/contracts/RequestManager.sol
+++ contracts/contracts/RequestManager.sol
@@ -28,14 +28,10 @@ contract RequestManager is Ownable {
     struct Claim {
         uint256 requestId;
         address claimer;
-        bool withdrawn;
-        uint256 termination;
-    }
-
-    struct Challenge {
+        uint256 claimerStake;
         address challenger;
         uint256 challengerStake;
-        uint256 claimerStake;
+        bool withdrawn;
         uint256 termination;
     }
 
@@ -57,10 +53,13 @@ contract RequestManager is Ownable {
         uint256 requestId
     );
 
-    event ClaimCreated(
-        uint256 claimId,
+    event ClaimMade(
         uint256 requestId,
+        uint256 claimId,
         address claimer,
+        uint256 claimerStake,
+        address challenger,
+        uint256 challengerStake,
         uint256 termination
     );
 
@@ -70,16 +69,6 @@ contract RequestManager is Ownable {
         address claimReceiver
     );
 
-    event ClaimChallenged(
-        uint256 claimId,
-        address challenger
-    );
-
-    event ChallengeCountered(
-        uint256 claimId,
-        address leader,
-        uint256 highestBid
-    );
 
     // Constants
     uint256 public claimStake;
@@ -94,8 +83,6 @@ contract RequestManager is Ownable {
 
     mapping (uint256 => Request) public requests;
     mapping (uint256 => Claim) public claims;
-    // claimId -> Challenge
-    mapping (uint256 => Challenge) public challenges;
 
     uint256 public gasPrice = 5e9;
     uint256 serviceFeePPM = 45_000;  //4.5%
@@ -105,10 +92,9 @@ contract RequestManager is Ownable {
 
     // The optimizer should take care of eval'ing this
     function gasReimbursementFee() public view returns (uint256) {
-        // TODO: update those values
         uint256 fillGas = 67105;
-        uint256 claimGas = 126402;
-        uint256 withdrawGas = 61954;
+        uint256 claimGas = 154634;
+        uint256 withdrawGas = 64081;
 
         return (fillGas + claimGas + withdrawGas) * gasPrice;
     }
@@ -226,71 +212,74 @@ contract RequestManager is Ownable {
         Request storage request = requests[requestId];
 
         require(!request.depositWithdrawn, "Deposit already withdrawn");
-        require(msg.value == claimStake, "Stake provided not correct");
+        require(msg.value == claimStake, "Invalid stake amount");
 
         request.activeClaims += 1;
         claimCounter += 1;
 
-        Claim storage newClaim = claims[claimCounter];
-        newClaim.requestId = requestId;
-        newClaim.claimer = msg.sender;
-        newClaim.withdrawn = false;
-        newClaim.termination = block.timestamp + claimPeriod;
+        Claim storage claim = claims[claimCounter];
+        claim.requestId = requestId;
+        claim.claimer = msg.sender;
+        claim.claimerStake = claimStake;
+        claim.challenger = address(0);
+        claim.challengerStake = 0;
+        claim.withdrawn = false;
+        claim.termination = block.timestamp + claimPeriod;
 
-        emit ClaimCreated(
-            claimCounter,
+        emit ClaimMade(
             requestId,
-            newClaim.claimer,
-            newClaim.termination
+            claimCounter,
+            claim.claimer,
+            claim.claimerStake,
+            claim.challenger,
+            claim.challengerStake,
+            claim.termination
         );
 
         return claimCounter;
     }
 
     function challengeClaim(uint256 claimId) external validClaimId(claimId) payable {
-        Challenge storage challenge = challenges[claimId];
-
-        require(challenge.challenger == address(0), "Already challenged");
-        require(block.timestamp < claims[claimId].termination, "Already claimed successfully");
-        require(msg.value > claimStake, "Not enough funds provided");
-
-        challenge.challenger = msg.sender;
-        challenge.challengerStake = msg.value;
-        challenge.claimerStake = claimStake;
-        challenge.termination = block.timestamp + challengePeriod;
-
-        emit ClaimChallenged(
-            claimId,
-            msg.sender
-        );
-    }
-
-    function counterChallenge(uint256 claimId) external validClaimId(claimId) payable {
         Claim storage claim = claims[claimId];
-        Challenge storage challenge = challenges[claimId];
-        require(challenge.challenger != address(0), "Claim not yet challenged");
-        require(msg.sender == claim.claimer || msg.sender == challenge.challenger, "Already challenged by another address");
-        require(block.timestamp < challenge.termination, "Challenge period finished");
+        require(block.timestamp < claim.termination, "Claim expired");
 
-        bool claimerStakeBigger = challenge.claimerStake > challenge.challengerStake;
-        address nextActor = claimerStakeBigger ? challenge.challenger : claim.claimer;
-        require(msg.sender == nextActor, "Not eligible to outbid");
+        address nextActor;
+        uint256 minValue;
+        uint256 periodExtension;
 
-        uint256 minStake = claimerStakeBigger ? challenge.claimerStake - challenge.challengerStake : challenge.challengerStake - challenge.claimerStake;
-        require(msg.value > minStake, "Not enough funds provided");
-
-        if (msg.sender == claim.claimer) {
-            challenge.claimerStake += msg.value;
+        if (claim.claimerStake > claim.challengerStake) {
+            if (claim.challenger == address(0)) {
+                claim.challenger = msg.sender;
+                periodExtension = challengePeriod;
+            } else {
+                periodExtension = challengePeriodExtension;
+            }
+            nextActor = claim.challenger;
+            minValue = claim.claimerStake - claim.challengerStake;
         } else {
-            challenge.challengerStake += msg.value;
+            nextActor = claim.claimer;
+            minValue = claim.challengerStake - claim.claimerStake;
         }
 
-        challenge.termination = Math.max(challenge.termination, block.timestamp + challengePeriodExtension);
+        require(msg.sender == nextActor, "Not eligible to outbid");
+        require(msg.value > minValue, "Not enough funds provided");
 
-        emit ChallengeCountered(
+        if (nextActor == claim.claimer) {
+            claim.claimerStake += msg.value;
+        } else {
+            claim.challengerStake += msg.value;
+        }
+
+        claim.termination = Math.max(claim.termination, block.timestamp + periodExtension);
+
+        emit ClaimMade(
+            claim.requestId,
             claimId,
-            msg.sender,
-            Math.max(challenge.challengerStake, challenge.claimerStake)
+            claim.claimer,
+            claim.claimerStake,
+            claim.challenger,
+            claim.challengerStake,
+            claim.termination
         );
     }
 
@@ -300,35 +289,28 @@ contract RequestManager is Ownable {
         require(!claim.withdrawn, "Claim already withdrawn");
 
         bool depositWithdrawn = request.depositWithdrawn;
+        bool claimChallenged = claim.challenger != address(0);
 
-        bool claimChallenged = challenges[claimId].termination != 0;
+        require(depositWithdrawn || block.timestamp >= claim.termination, "Claim period not finished");
+
         address claimReceiver;
         if (!claimChallenged) {
-            require(depositWithdrawn || block.timestamp >= claim.termination, "Claim period not finished");
             claimReceiver = claim.claimer;
         } else {
-            require(depositWithdrawn || block.timestamp >= challenges[claimId].termination, "Challenge period not finished");
             // check if l1 resolved
-            Challenge storage challenge = challenges[claimId];
-
-            bool claimerStakeBigger = challenge.claimerStake > challenge.challengerStake;
-            claimReceiver = claimerStakeBigger ? claim.claimer : challenge.challenger;
+            claimReceiver = claim.claimerStake > claim.challengerStake ? claim.claimer : claim.challenger;
         }
 
         claim.withdrawn = true;
         request.activeClaims -= 1;
 
-        uint256 ethToTransfer = claimStake;
         if (!depositWithdrawn && claimReceiver == claim.claimer) {
             request.depositWithdrawn = true;
             withdraw_deposit(claimId, request, claim, claimReceiver);
         }
         // The claim is set the `withdrawn` state above, so the following effects
         // needs to happen afterwards to avoid reentrency problems
-        if (claimChallenged) {
-            ethToTransfer += withdraw_challenge(claimId);
-        }
-
+        uint256 ethToTransfer = claim.claimerStake + claim.challengerStake;
         (bool sent,) = claimReceiver.call{value: ethToTransfer}("");
         require(sent, "Failed to send Ether");
 
@@ -354,19 +336,6 @@ contract RequestManager is Ownable {
 
         (bool sent,) = claimReceiver.call{value: request.lpFee}("");
         require(sent, "Failed to send Ether");
-    }
-
-    function withdraw_challenge(
-        uint256 claimId
-    ) private returns (uint256) {
-        Challenge storage challenge = challenges[claimId];
-        uint256 challengeStake = challenge.claimerStake + challenge.challengerStake;
-        // This should never happen, but the check is cheap
-        require(challengeStake >= claimStake, "Challenge stake too small");
-
-        delete challenges[claimId];
-
-        return challengeStake - claimStake;
     }
 
     function withdrawRaisyncFees() external onlyOwner {
