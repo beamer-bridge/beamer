69dac7ae41c15f23a04831b6bce9359ed548ef9f
Ivan Stanković <ivan@brainbot.com>
2022-01-18 12:32:58 +0100

    contracts: update tests

--- contracts/tests/test_request_manager.py
+++ contracts/tests/test_request_manager.py
@@ -9,15 +9,15 @@ def test_claim_with_different_stakes(token, request_manager, claim_stake):
     request_id = make_request(request_manager, token, accounts[0], 1)
 
     claim = request_manager.claimRequest(request_id, {"from": accounts[0], "value": claim_stake})
-    assert "ClaimCreated" in claim.events
+    assert "ClaimMade" in claim.events
 
-    with brownie.reverts("Stake provided not correct"):
+    with brownie.reverts("Invalid stake amount"):
         request_manager.claimRequest(request_id, {"from": accounts[0], "value": claim_stake - 1})
 
-    with brownie.reverts("Stake provided not correct"):
+    with brownie.reverts("Invalid stake amount"):
         request_manager.claimRequest(request_id, {"from": accounts[0], "value": claim_stake + 1})
 
-    with brownie.reverts("Stake provided not correct"):
+    with brownie.reverts("Invalid stake amount"):
         request_manager.claimRequest(request_id, {"from": accounts[0]})
 
 
@@ -39,9 +39,9 @@ def test_claim_challenge(request_manager, token, claim_stake):
     challenge = request_manager.challengeClaim(
         claim.return_value, {"from": accounts[1], "value": claim_stake + 1}
     )
-    assert "ClaimChallenged" in challenge.events
+    assert "ClaimMade" in challenge.events
 
-    with brownie.reverts("Already challenged"):
+    with brownie.reverts("Not eligible to outbid"):
         request_manager.challengeClaim(
             claim.return_value, {"from": accounts[1], "value": claim_stake + 1}
         )
@@ -56,32 +56,32 @@ def test_claim_counter_challenge(request_manager, token, claim_stake):
     claim = request_manager.claimRequest(request_id, {"from": claimer, "value": claim_stake})
     claim_id = claim.return_value
 
-    with brownie.reverts("Claim not yet challenged"):
-        request_manager.counterChallenge(claim_id, {"from": accounts[2], "value": claim_stake})
+    with brownie.reverts("Not enough funds provided"):
+        request_manager.challengeClaim(claim_id, {"from": accounts[2], "value": claim_stake})
 
     # Do a proper challenge
     request_manager.challengeClaim(claim_id, {"from": challenger, "value": claim_stake + 1})
 
     # Another party must not be able to join the challenge game
-    with brownie.reverts("Already challenged by another address"):
-        request_manager.counterChallenge(claim_id, {"from": accounts[2]})
+    with brownie.reverts("Not eligible to outbid"):
+        request_manager.challengeClaim(claim_id, {"from": accounts[2]})
 
     # The sender of the last challenge must not be able to challenge again
     with brownie.reverts("Not eligible to outbid"):
-        request_manager.counterChallenge(claim_id, {"from": challenger})
+        request_manager.challengeClaim(claim_id, {"from": challenger})
 
     # The other party must be able to re-challenge
-    outbid = request_manager.counterChallenge(claim_id, {"from": claimer, "value": 2})
-    assert "ChallengeCountered" in outbid.events
+    outbid = request_manager.challengeClaim(claim_id, {"from": claimer, "value": 2})
+    assert "ClaimMade" in outbid.events
 
     # Check that the roles are reversed now
     with brownie.reverts("Not eligible to outbid"):
-        request_manager.counterChallenge(claim_id, {"from": claimer, "value": 1})
+        request_manager.challengeClaim(claim_id, {"from": claimer, "value": 1})
 
     # The other party must be able to re-challenge, but must increase the stake
     with brownie.reverts("Not enough funds provided"):
-        request_manager.counterChallenge(claim_id, {"from": challenger, "value": 1})
-    request_manager.counterChallenge(claim_id, {"from": challenger, "value": 2})
+        request_manager.challengeClaim(claim_id, {"from": challenger, "value": 1})
+    request_manager.challengeClaim(claim_id, {"from": challenger, "value": 2})
 
 
 def test_claim_period_extension(
@@ -95,30 +95,29 @@ def test_claim_period_extension(
     claim = request_manager.claimRequest(request_id, {"from": claimer, "value": claim_stake})
     claim_id = claim.return_value
 
-    assert claim.timestamp + claim_period == request_manager.claims(claim_id)[3]
+    assert claim.timestamp + claim_period == request_manager.claims(claim_id)[6]
 
     challenge = request_manager.challengeClaim(
         claim_id, {"from": challenger, "value": claim_stake + 1}
     )
-    assert challenge.timestamp + challenge_period == request_manager.challenges(claim_id)[3]
+    assert challenge.timestamp + challenge_period == request_manager.claims(claim_id)[6]
 
     # Another challenge with big margin to the end of the termination
     # shouldn't increase the termination
-    request_manager.counterChallenge(claim_id, {"from": claimer, "value": 2})
-    assert challenge.timestamp + challenge_period == request_manager.challenges(claim_id)[3]
+    request_manager.challengeClaim(claim_id, {"from": claimer, "value": 2})
+    assert challenge.timestamp + challenge_period == request_manager.claims(claim_id)[6]
 
     # Timetravel close to end of challenge period
     chain.mine(timedelta=challenge_period * 8 / 10)
-    rechallenge = request_manager.counterChallenge(claim_id, {"from": challenger, "value": 2})
+    rechallenge = request_manager.challengeClaim(claim_id, {"from": challenger, "value": 2})
     assert (
-        rechallenge.timestamp + challenge_period_extension
-        == request_manager.challenges(claim_id)[3]
+        rechallenge.timestamp + challenge_period_extension == request_manager.claims(claim_id)[6]
     )
 
     # Timetravel over the end of challenge period
     chain.mine(timedelta=challenge_period_extension)
-    with brownie.reverts("Challenge period finished"):
-        request_manager.counterChallenge(claim_id, {"from": claimer, "value": 2})
+    with brownie.reverts("Claim expired"):
+        request_manager.challengeClaim(claim_id, {"from": claimer, "value": 2})
 
 
 def test_withdraw_nonexistent_claim(request_manager):
@@ -212,7 +211,7 @@ def test_withdraw_with_challenge(request_manager, token, claim_stake, challenge_
     assert web3.eth.get_balance(challenger.address) == challenger_eth_balance - claim_stake - 1
 
     # Withdraw must fail when claim pariod is not over
-    with brownie.reverts("Challenge period not finished"):
+    with brownie.reverts("Claim period not finished"):
         request_manager.withdraw(claim_id, {"from": claimer})
 
     # Timetravel after claim period
@@ -433,7 +432,7 @@ def test_withdraw_with_two_claims_first_unsuccessful_then_successful(
     assert web3.eth.get_balance(challenger.address) == challenger_eth_balance - claim_stake - 1
 
     # Withdraw must fail when claim pariod is not over
-    with brownie.reverts("Challenge period not finished"):
+    with brownie.reverts("Claim period not finished"):
         request_manager.withdraw(claim1_id, {"from": claimer1})
 
     # Timetravel after claim period
