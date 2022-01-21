a7f1aeb6c109787797ec04b87d66bbb4ae19c56e
Paul Lange <palango@gmx.de>
2022-01-21 12:55:05 +0100

    frontend: Send fee with request

--- frontend/src/services/transactions/token.ts
+++ frontend/src/services/transactions/token.ts
@@ -2,14 +2,14 @@ import { JsonRpcSigner } from '@ethersproject/providers';
 import { BigNumber, Contract } from 'ethers';
 import { DeepReadonly } from 'vue';
 
-import CustomToken from '@/assets/CustomToken.json';
+import StandardToken from '@/assets/StandardToken.json';
 
 export async function ensureTokenAllowance(
   signer: DeepReadonly<JsonRpcSigner>,
   tokenAddress: string,
   amount: BigNumber,
 ): Promise<void> {
-  const tokenContract = new Contract(tokenAddress, CustomToken.abi, signer);
+  const tokenContract = new Contract(tokenAddress, StandardToken.abi, signer);
   const signerAddress = await signer.getAddress();
   const requestManagerAddress = process.env.VUE_APP_REQUEST_MANAGER_ADDRESS;
   const allowance: BigNumber = await tokenContract.allowance(signerAddress, requestManagerAddress);
