e5cd086b58eb20a26c21cc5bbb59201205e0e075
Paul Lange <palango@gmx.de>
2022-02-01 15:08:12 +0100

    contracts: Fix the deployment script

--- contracts/scripts/deploy.py
+++ contracts/scripts/deploy.py
@@ -1,11 +1,14 @@
 from brownie import (
-    DummyProofSubmitter,
     FillManager,
     MintableToken,
+    OptimismProofSubmitter,
     RequestManager,
     ResolutionRegistry,
+    Resolver,
+    TestCrossDomainMessenger,
     Wei,
     accounts,
+    web3,
 )
 
 
@@ -13,11 +16,11 @@ def main() -> None:
     accounts.add("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80")
     deployer = accounts.at("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266")
 
-    l1_resolver_address = "0x0000000000000000000000000000000000000001"
+    messenger = TestCrossDomainMessenger.deploy({"from": deployer})
+    resolver = Resolver.deploy(messenger.address, {"from": deployer})
+    resolution_registry = ResolutionRegistry.deploy({"from": deployer})
 
     MintableToken.deploy(int(1e18), {"from": deployer})
-
-    resolution_registry = ResolutionRegistry.deploy({"from": deployer})
 
     claim_stake = Wei("0.01 ether")
     claim_period = 60 * 60  # 1 hour
@@ -34,5 +37,8 @@ def main() -> None:
         {"from": deployer},
     )
 
-    proof_submitter = DummyProofSubmitter.deploy({"from": deployer})
-    FillManager.deploy(l1_resolver_address, proof_submitter.address, {"from": deployer})
+    proof_submitter = OptimismProofSubmitter.deploy(messenger.address, {"from": deployer})
+    f = FillManager.deploy(resolver.address, proof_submitter.address, {"from": deployer})
+    f.addAllowedLP(deployer.address, {"from": deployer})
+
+    resolver.addRegistry(web3.eth.chain_id, resolution_registry.address, {"from": deployer})
