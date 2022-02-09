f01927a5332dcdc676013e6ae7b3d642c91d0164
Ivan Stanković <ivan@brainbot.com>
2022-02-09 15:47:55 +0100

    contracts: tests: add a test for restricted calls

--- contracts/tests/test_l1_resolution.py
+++ contracts/tests/test_l1_resolution.py
@@ -1,3 +1,4 @@
+import brownie
 from brownie import accounts, web3
 from web3.constants import ADDRESS_ZERO
 
@@ -21,3 +22,27 @@ def test_l1_end_to_end(fill_manager, deployer, resolution_registry, token, resol
         chain_id, request_id, token.address, receiver, amount, {"from": filler}
     )
     assert resolution_registry.eligibleClaimers(request_id) == filler
+
+
+def test_restricted_calls(contracts, resolver):
+    """Test that important contract calls cannot be invoked by a random caller."""
+    caller = accounts[3]
+
+    # fill_manager -> proof_submitter -> messenger1 -> L1 resolver ->
+    # messenger2 -> resolution registry
+    with brownie.reverts("RestrictedCalls: unknown caller"):
+        contracts.proof_submitter.submitProof(
+            resolver, 0, brownie.chain.id, caller, {"from": caller}
+        )
+
+    with brownie.reverts("RestrictedCalls: unknown caller"):
+        contracts.messenger1.sendMessage(resolver, b"\x00", 1_000_000, {"from": caller})
+
+    with brownie.reverts("RestrictedCalls: unknown caller"):
+        contracts.resolver.resolve(0, brownie.chain.id, brownie.chain.id, caller, {"from": caller})
+
+    with brownie.reverts("RestrictedCalls: unknown caller"):
+        contracts.messenger2.sendMessage(resolver, b"\x00", 1_000_000, {"from": caller})
+
+    with brownie.reverts("RestrictedCalls: unknown caller"):
+        contracts.resolution_registry.resolveRequest(0, caller, {"from": caller})
