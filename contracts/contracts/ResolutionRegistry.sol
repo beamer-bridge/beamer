cf8e916afba7057800beece20473cf696286b0f8
Paul Lange <palango@gmx.de>
2022-02-15 15:14:36 +0100

    contracts: Add L1 chain id parameter to resolution registry

--- contracts/contracts/ResolutionRegistry.sol
+++ contracts/contracts/ResolutionRegistry.sol
@@ -12,8 +12,8 @@ contract ResolutionRegistry is CrossDomainRestrictedCalls {
 
     mapping(bytes32 => address) public eligibleClaimers;
 
-    function resolveRequest(bytes32 requestHash, address eligibleClaimer)
-        external restricted(block.chainid, msg.sender) {
+    function resolveRequest(bytes32 requestHash, uint256 resolutionChainId, address eligibleClaimer)
+        external restricted(resolutionChainId, msg.sender) {
 
         require(eligibleClaimers[requestHash] == address(0), "Resolution already recorded");
         eligibleClaimers[requestHash] = eligibleClaimer;
