941eed85edda239496ac078d27abea2d9f23b7ca
nekhaly <nekhaly@gmail.com>
2022-02-24 10:26:07 +0100

    Introduce request processing component
    This adds requestProcessing screen after pressing transfer funds button
    on request form. As I used DaisyUI for Stepping component, I found that
    it conflicts with Ripple lib so I disabled it from tailwind config file

---
+++ frontend/src/components/RequestProcessing.vue
@@ -0,0 +1,35 @@
+<template>
+  <Card class="bg-teal-light p-10">
+    <div class="flex flex-col justify-center items-center gap-4 text-black text-lg">
+      <div class="flex flex-col justify-center items-center">
+        <div>Sending <span>18.00</span> <span>DAI</span></div>
+        <div>from <span>Goerli</span></div>
+        <div>to <span>Goerli</span></div>
+      </div>
+      <div class="flex flex-col justify-center items-center">
+        <div>Recipient address</div>
+        <div>0x1111111111111111111111111111111</div>
+      </div>
+    </div>
+  </Card>
+  <div class="flex flex-col justify-center items-center">
+    <div>
+      <ul class="steps steps-vertical">
+        <li data-content="" class="step step-success">Please confirm on Metamask</li>
+        <li data-content="" class="step step-success">Request is being fulfilled</li>
+        <li data-content="" class="step">
+          <div class="flex flex-col justify-start items-start">
+            <div>Please switch chain connection to verify funds reception</div>
+            <div class="text-orange-light">Chain switch failed. Try again.</div>
+          </div>
+        </li>
+        <li data-content="" class="step">Verifying...</li>
+        <li data-content="" class="step">Transfer completed</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Card from './layout/Card.vue';
+</script>
