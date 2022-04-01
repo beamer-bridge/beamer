494c9657c4b9b4b3855b6de2769df03c0144f5cf
nekhaly <nekhaly@gmail.com>
2022-04-01 18:56:58 +0200

    Fix uncompleted request on frontend
    After changing configuration it appears that we were using the same
    token address of source rollup for both source and target token address,
    as they were equal anyway, so the issue didn't appear before they are
    different in new condiguration. As there's no way to map source and
    target token addresses, we used the token symbol temporarily for this
    mapping until we figure out another proepr way to make this.
    Also the unnecassry event check at the request end was causing issue,
    it has been removed anyway.

--- frontend/src/services/transactions/request-manager.ts
+++ frontend/src/services/transactions/request-manager.ts
@@ -1,21 +1,10 @@
-import { JsonRpcSigner, TransactionReceipt, TransactionResponse } from '@ethersproject/providers';
+import { JsonRpcSigner, TransactionResponse } from '@ethersproject/providers';
 import { Contract } from 'ethers';
 import { DeepReadonly, Ref } from 'vue';
 
 import RequestManager from '@/assets/RequestManager.json';
 import { EthereumProvider } from '@/services/web3-provider';
 import { Request, RequestState } from '@/types/data';
-
-function findFirstEvent(receipt: TransactionReceipt, eventName: string) {
-  const isRequestCreated = (e) => {
-    if ('undefined' === typeof e['event']) {
-      return false;
-    }
-    console.log(e.event);
-    return e.event === eventName;
-  };
-  return receipt.events.find(isRequestCreated);
-}
 
 export async function getRequestFee(
   ethereumProvider: Readonly<EthereumProvider>,
@@ -40,7 +29,7 @@ export async function sendRequestTransaction(
   }
 
   const requestManagerContract = new Contract(
-    request.requestManagerAddress,
+    request.requestManagerAddress as string,
     RequestManager.abi,
     signer,
   );
@@ -68,12 +57,6 @@ export async function sendRequestTransaction(
 
   const transactionReceipt = await transaction.wait();
   request.receipt = transactionReceipt;
-  //events is added dynamically when wait method called!
-  // TODO verification of other args?
-  const event = findFirstEvent(request.receipt, 'RequestCreated');
-  if (!event) {
-    //TODO error
-  }
-  request.requestId = event.args.requestId;
+
   return request;
 }
