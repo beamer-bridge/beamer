7c891ea4d14e96e5b432dd539724f6e8925a2375
nekhaly <nekhaly@gmail.com>
2022-04-06 12:20:25 +0200

    Fix network change error in progress screen
    If user was in progress screen and then changed network from metamask,
    when trying to make a new transfer, a change network error occurs. This
    happens because once network is changed, page should be reloaded, and
    we are applying this action only in form screen. We moved the watcher to
    the parent RequestDialog component and it should work on both screens
    now. The side effect of this is that, progress screen reloads to form
    screen on changing networks, but I think this's acceptable for the sake
    of avoiding the error. In future, once we have a proper state managemnet
    , we can use it to keep the UI state as well so we avoid this reload
    side effects.

--- frontend/src/components/RequestDialog.vue
+++ frontend/src/components/RequestDialog.vue
@@ -170,7 +170,7 @@ const submitRequestTransaction = async (formResult: {
 };
 
 watch(ethereumProvider.value.chainId, async () => {
-  await executeGetFee();
+  location.reload();
 });
 executeGetFee();
 
