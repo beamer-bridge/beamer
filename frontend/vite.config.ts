a4417d626610b73127865344f9242f2a17277517
Thore Strassburg <thore@weilbier.net>
2022-04-20 13:54:40 +0200

    add test coverage measurement
    Having Vitest it is just too easy to add test coverage to not to do it.
    So far only the `test:coverage` reports the results on a manual call. We
    will have to add this later to our continuous integration pipeline too.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,17 +3,27 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import { defineConfig } from 'vite';
 
+const source_directory = path.resolve(__dirname, 'src');
+const test_directory = path.resolve(__dirname, 'tests');
+const test_output_directory = path.resolve(test_directory, 'output');
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '~': path.resolve(__dirname, './tests'),
+      '@': source_directory,
+      '~': test_directory,
     },
   },
   test: {
     globals: true,
     reporters: ['default', 'junit'],
+    outputFile: path.resolve(test_output_directory, 'junit.xml'),
     mockClear: true,
+    coverage: {
+      all: true,
+      src: source_directory,
+      reportDir: path.resolve(test_output_directory, 'coverage'),
+    },
   },
 });
