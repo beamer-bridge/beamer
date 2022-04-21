0fe9f7c7894c85e20a3812f41dc50b915ebbbf09
manuelwedler <manuel.wedler@googlemail.com>
2022-04-21 16:28:54 +0200

    frontend: Remove unused code from composables

--- frontend/src/composables/useWaitForRequestFulfilment.ts
+++ frontend/src/composables/useWaitForRequestFulfilment.ts
@@ -5,7 +5,6 @@ import { ref } from 'vue';
 import { listenOnFulfillment } from '@/services/transactions/fill-manager';
 import type { Request } from '@/types/data';
 import { RequestState } from '@/types/data';
-import createAsyncProcess from '@/utils/create-async-process';
 
 export function useWaitForRequestFulfilment() {
   const error = ref<string | undefined>(undefined);
@@ -30,6 +29,5 @@ export function useWaitForRequestFulfilment() {
     requestState.value = RequestState.RequestSuccessful;
   };
 
-  const { active, run } = createAsyncProcess(waitForRequestFulfilment);
-  return { run, active, error };
+  return { run: waitForRequestFulfilment };
 }
