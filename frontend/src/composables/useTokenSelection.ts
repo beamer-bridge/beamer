4ce4c0b0516f00dd56980c72bd128255193de999
manuelwedler <manuel.wedler@googlemail.com>
2022-05-03 11:45:12 +0200

    frontend: Refactor token selection to composable
    Makes RequestFormInput much simpler, because all token related logic
    can be seen as one unit that can be imported from a composable.
    
    Part of extracting logical units from the components to composables in order to have cleaner code

---
+++ frontend/src/composables/useTokenSelection.ts
@@ -0,0 +1,59 @@
+import { computed, Ref, ref } from 'vue';
+
+import { getTokenDecimals } from '@/services/transactions/token';
+import type { EthereumProvider } from '@/services/web3-provider';
+import { ChainConfigMapping, Token } from '@/types/config';
+import { SelectorOption } from '@/types/form';
+
+export function useTokenSelection(
+  provider: Ref<EthereumProvider | undefined>,
+  chains: Ref<ChainConfigMapping>,
+) {
+  const tokens = computed(() =>
+    chains.value[String(provider.value?.chainId.value)]?.tokens.map((token) =>
+      getTokenSelectorOption(token),
+    ),
+  );
+
+  const selectedToken: Ref<SelectorOption | undefined> = ref();
+  const selectedTokenAddress = computed(() => selectedToken.value?.value);
+
+  const switchToken = (token: SelectorOption) => {
+    selectedToken.value = token;
+  };
+
+  const addTokenToProvider = async () => {
+    if (!provider.value || !selectedToken.value) {
+      throw new Error('Provider or token missing!');
+    }
+    try {
+      const decimals = await getTokenDecimals(provider.value, selectedToken.value.value);
+
+      await provider.value.addToken({
+        address: selectedToken.value.value,
+        symbol: selectedToken.value.label,
+        decimals: Number(decimals),
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const addTokenAvailable = computed(() => provider.value && selectedToken.value);
+
+  return {
+    selectedToken,
+    selectedTokenAddress,
+    tokens,
+    switchToken,
+    addTokenToProvider,
+    addTokenAvailable,
+  };
+}
+
+function getTokenSelectorOption(token: Token): SelectorOption {
+  return {
+    value: token.address,
+    label: token.symbol,
+  };
+}
