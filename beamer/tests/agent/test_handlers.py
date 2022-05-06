f302fa04db18aa8da1b489cd806714b777de7bcb
Paul Lange <palango@gmx.de>
2022-05-06 11:37:27 +0200

    tests: Don't use deployed token contract in unit tests
    This speeds up the tests a bit. The next step would be to construct
    config that doesn't use deployed contracts to avoid touching the
    blockchain all together.

--- beamer/tests/agent/test_handlers.py
+++ beamer/tests/agent/test_handlers.py
@@ -47,14 +47,14 @@ def make_address() -> ChecksumAddress:
     return to_checksum_address(make_bytes(20))
 
 
-def make_request(token) -> Request:
+def make_request() -> Request:
     return Request(
         request_id=REQUEST_ID,
         source_chain_id=SOURCE_CHAIN_ID,
         target_chain_id=TARGET_CHAIN_ID,
-        source_token_address=token.address,
-        target_token_address=token.address,
-        target_address=token.address,
+        source_token_address=make_address(),
+        target_token_address=make_address(),
+        target_address=make_address(),
         amount=TokenAmount(123),
         valid_until=456,
     )
@@ -105,8 +105,8 @@ def make_context(config: Config, request_manager: Contract = None):
     )
 
 
-def test_skip_not_self_filled(token, config: Config):
-    request = make_request(token)
+def test_skip_not_self_filled(config: Config):
+    request = make_request()
     context = make_context(config)
 
     context.requests.add(request.id, request)
@@ -116,8 +116,8 @@ def test_skip_not_self_filled(token, config: Config):
     assert request.is_pending  # pylint:disable=no-member
 
 
-def test_ignore_expired(token, config: Config):
-    request = make_request(token)
+def test_ignore_expired(config: Config):
+    request = make_request()
     request.filler = config.account.address
 
     context = make_context(config)
@@ -128,8 +128,8 @@ def test_ignore_expired(token, config: Config):
     assert request.is_ignored  # pylint:disable=no-member
 
 
-def test_request_garbage_collection_without_claim(token, config: Config):
-    request = make_request(token)
+def test_request_garbage_collection_without_claim(config: Config):
+    request = make_request()
     request.fill(config.account.address, b"", b"")
     request.withdraw()
 
@@ -140,7 +140,7 @@ def test_request_garbage_collection_without_claim(token, config: Config):
     process_requests(context)
     assert len(context.requests) == 0
 
-    request = make_request(token)
+    request = make_request()
     request.ignore()
     context.requests.add(request.id, request)
 
@@ -149,8 +149,8 @@ def test_request_garbage_collection_without_claim(token, config: Config):
     assert len(context.requests) == 0
 
 
-def test_request_garbage_collection_with_claim(token, config: Config):
-    request = make_request(token)
+def test_request_garbage_collection_with_claim(config: Config):
+    request = make_request()
     request.fill(config.account.address, b"", b"")
     request.withdraw()
 
@@ -178,8 +178,8 @@ def test_request_garbage_collection_with_claim(token, config: Config):
     assert len(context.claims) == 0
 
 
-def test_handle_initiate_l1_resolution(token: Contract, config: Config):
-    request = make_request(token)
+def test_handle_initiate_l1_resolution(config: Config):
+    request = make_request()
 
     context = make_context(config)
     context.requests.add(request.id, request)
@@ -207,7 +207,7 @@ def test_handle_initiate_l1_resolution(token: Contract, config: Config):
     assert context.resolution_pool.submit.called  # pylint:disable=no-member
 
 
-def test_handle_request_resolved(token: Contract, config: Config):
+def test_handle_request_resolved(config: Config):
     context = make_context(config)
     filler = make_address()
 
@@ -223,7 +223,7 @@ def test_handle_request_resolved(token: Contract, config: Config):
     assert process_event(event, context) == (False, None)
 
     # Must store the result in the request
-    request = make_request(token)
+    request = make_request()
     context.requests.add(request.id, request)
 
     assert request.l1_resolution_filler is None
@@ -231,10 +231,10 @@ def test_handle_request_resolved(token: Contract, config: Config):
     assert request.l1_resolution_filler == filler
 
 
-def test_handle_claim_made(token: Contract, config: Config):
+def test_handle_claim_made(config: Config):
     context = make_context(config)
 
-    request = make_request(token)
+    request = make_request()
     request.fill(config.account.address, b"", b"")
     context.requests.add(request.id, request)
 
