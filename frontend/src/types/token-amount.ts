9fe9cf50a2c1e40fb61f93dc4f9fe3c2b07d9871
Thore Strassburg <thore@weilbier.net>
2022-05-11 16:38:15 +0200

    frontend: add new token amount type
    This is a nice convinience type that helps to interact with with token balances
    and amounts. Therefore it combines the usage of the new `UInt256` type and
    a specific token. The hereby most important information by the token is the
    number of decimals. This gets used to parse user inputs as well as format
    amounts properly.
    
    After all this types hides some complexity and makes it easy to work, without
    having to remember what a specific `UInt256` is about, what token relates to it,
    calling `ethers` utility functions everywhere and so on.
    
    Furthermore this type also follows the encoding rules and can be persisted as it
    necessary to be used with a transfer.

---
+++ frontend/src/types/token-amount.ts
@@ -0,0 +1,43 @@
+import type { Token } from '@/types/data';
+import type { Encodable } from '@/types/encoding';
+import type { UInt256Data } from '@/types/uint-256';
+import { UInt256 } from '@/types/uint-256';
+
+export class TokenAmount implements Encodable<TokenAmountData> {
+  private token: Token;
+  private amount: UInt256;
+
+  constructor(data: TokenAmountData) {
+    this.token = data.token;
+    this.amount = new UInt256(data.amount);
+  }
+
+  static parse(value: string, token: Token) {
+    const amount = UInt256.parse(value, token.decimals).encode();
+    return new this({ amount, token });
+  }
+
+  get uint256(): UInt256 {
+    return this.amount;
+  }
+
+  get decimalAmount(): string {
+    return this.amount.format(this.token.decimals);
+  }
+
+  get formattedAmount(): string {
+    return `${this.decimalAmount} ${this.token.symbol}`;
+  }
+
+  public encode(): TokenAmountData {
+    return {
+      token: this.token,
+      amount: this.amount.encode(),
+    };
+  }
+}
+
+export type TokenAmountData = {
+  token: Token;
+  amount: UInt256Data;
+};
