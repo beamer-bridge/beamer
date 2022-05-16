9af4fe2601f6c2d6804c1ccd656e1f223a1fed36
Frederik Lührs <luehrs.fred@gmail.com>
2022-05-16 11:50:19 +0200

    tests: add test for resolution registry behavior

---
+++ beamer/tests/contracts/test_resolution_registry.py
@@ -0,0 +1,49 @@
+import brownie
+from brownie import web3
+from eth_utils import to_bytes
+from web3.constants import ADDRESS_ZERO
+
+from beamer.tests.agent.utils import make_address
+from beamer.tests.util import create_fill_hash
+
+
+def test_invalidation_before_and_after_resolution(contracts, token, resolution_registry):
+    contracts.messenger2.setLastSender(contracts.resolver.address)
+
+    address = make_address()
+    request_id = 1
+    fill_id = to_bytes(b"123")
+    chain_id = web3.eth.chain_id
+
+    fill_hash = create_fill_hash(
+        request_id,
+        chain_id,
+        chain_id,
+        token.address,
+        address,
+        1,
+        fill_id,
+    )
+
+    assert not resolution_registry.invalidFillHashes(fill_hash)
+    resolution_registry.invalidateFillHash(
+        request_id, fill_hash, chain_id, {"from": contracts.messenger2}
+    )
+    # Fill hash must be invalidated
+    assert resolution_registry.invalidFillHashes(fill_hash)
+
+    assert resolution_registry.fillers(fill_hash) == ADDRESS_ZERO
+
+    resolution_registry.resolveRequest(
+        1, fill_hash, chain_id, address, {"from": contracts.messenger2}
+    )
+
+    # Resolution validates fill hash again
+    assert resolution_registry.fillers(fill_hash) == address
+    assert not resolution_registry.invalidFillHashes(fill_hash)
+
+    # Invalidation of a resolved request should fail
+    with brownie.reverts("Cannot invalidate resolved fillHashes"):
+        resolution_registry.invalidateFillHash(
+            request_id, fill_hash, chain_id, {"from": contracts.messenger2}
+        )
