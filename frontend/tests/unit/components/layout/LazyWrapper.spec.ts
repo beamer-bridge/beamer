0ece915700dfac4e163db7a0e20c2d8869f62a9f
Thore Strassburg <thore@weilbier.net>
2022-05-20 16:14:37 +0200

    frontend: add new lazy wrapper component
    The LazyWrapper component is a quite special one as it makes use of the
    intersection observer feature of modern browsers. Though it also supports
    outdated browsers. It can be used to wrap a component so that it will not be
    rendered before it becomes visible, or becomes close to be visible. This is
    especially useful for long lists. This is even more important for components
    that load data in background or similar.

---
+++ frontend/tests/unit/components/layout/LazyWrapper.spec.ts
@@ -0,0 +1,110 @@
+import { mount } from '@vue/test-utils';
+
+import LazyWrapper from '@/components/layout/LazyWrapper.vue';
+
+function createWrapper(options?: {
+  slot?: string;
+  minimumHeight?: string;
+  rootElement?: HTMLElement | null;
+  rootMargin?: string;
+  threshold?: number;
+}) {
+  return mount(LazyWrapper, {
+    shallow: true,
+    slots: {
+      default: options?.slot ?? '',
+    },
+    props: {
+      minimumHeight: options?.minimumHeight,
+      rootElement: options?.rootElement,
+      rootMargin: options?.rootMargin,
+      threshold: options?.threshold,
+    },
+  });
+}
+
+describe('LazyWrapper.vue', () => {
+  it('always shows the slot if browser does not support intersection observers', () => {
+    Object.defineProperty(global, 'window', {
+      writable: true,
+      value: {},
+    });
+    expect(global.window).not.toHaveProperty('IntersectionObserver');
+
+    const wrapper = createWrapper({ slot: 'test content' });
+
+    expect(wrapper.text()).toBe('test content');
+  });
+
+  it('creates observer with expected properties', () => {
+    const observerConstrutor = vi.fn().mockImplementation(() => ({ observe: vi.fn() }));
+
+    Object.defineProperty(global.window, 'IntersectionObserver', {
+      writable: true,
+      value: observerConstrutor,
+    });
+
+    createWrapper({
+      rootElement: null,
+      rootMargin: '2px',
+      threshold: 1.0,
+    });
+
+    expect(observerConstrutor).toHaveBeenCalledOnce();
+    expect(observerConstrutor).toHaveBeenLastCalledWith(expect.anything(), {
+      root: null,
+      rootMargin: '2px',
+      threshold: 1.0,
+    });
+  });
+
+  it('starts to observe top element', () => {
+    const observe = vi.fn();
+
+    Object.defineProperty(global.window, 'IntersectionObserver', {
+      writable: true,
+      value: vi.fn().mockImplementation(() => ({ observe })),
+    });
+
+    createWrapper();
+
+    expect(observe).toHaveBeenCalledOnce();
+    expect(observe).toHaveBeenLastCalledWith(expect.any(HTMLElement));
+  });
+
+  it('hides slot per default', () => {
+    Object.defineProperty(global.window, 'IntersectionObserver', {
+      writable: true,
+      value: vi.fn().mockImplementation(() => ({ observe: vi.fn() })),
+    });
+
+    const wrapper = createWrapper({ slot: 'test content' });
+
+    expect(wrapper.text()).toBe('');
+  });
+
+  it('shows and hides slot based on observer callback with visible state', async () => {
+    let observerCallback = vi.fn();
+
+    Object.defineProperty(global.window, 'IntersectionObserver', {
+      writable: true,
+      value: vi.fn().mockImplementation((callback) => {
+        observerCallback = callback;
+        return { observe: vi.fn() };
+      }),
+    });
+
+    const wrapper = createWrapper({ slot: 'test content' });
+    expect(wrapper.text()).toBe('');
+
+    observerCallback([{ isIntersecting: true }]);
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.text()).toBe('test content');
+
+    observerCallback([{ isIntersecting: false }]);
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.text()).toBe('');
+  });
+});
