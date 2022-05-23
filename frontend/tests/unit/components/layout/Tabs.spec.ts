6244e6981d2e909d3eba5fe2f625b915a5df3331
Thore Strassburg <thore@weilbier.net>
2022-05-23 14:23:32 +0200

    frontend: allow to programmatically change tabs
    This is an extension of the `Tabs` component that allows to switch the
    tabs not only by the user with clicking the headers, but also
    programmatically via a Vue component property.
    Note that it has been actively decided to not use a `v-model` approach.
    This makes the property optional. Nevertheless the component emits an
    event to allow the parent to keep in sync which tab is currently active.

--- frontend/tests/unit/components/layout/Tabs.spec.ts
+++ frontend/tests/unit/components/layout/Tabs.spec.ts
@@ -4,28 +4,25 @@ import { markRaw } from 'vue';
 import Tabs from '@/components/layout/Tabs.vue';
 import { getRandomString } from '~/utils/data_generators';
 
-async function createWrapper(options?: { tabs: Array<{ label?: string; template?: string }> }) {
+function createWrapper(options?: {
+  tabs: Array<{ label?: string; template?: string }>;
+  activeTabLabel?: string;
+}) {
   const tabs = (options?.tabs ?? []).map((tab) => ({
     label: tab.label ?? getRandomString(),
     content: markRaw({ template: tab.template ?? '<div />' }),
   }));
 
-  const wrapper = mount(Tabs, {
+  return mount(Tabs, {
     shallow: false,
-    props: { tabs },
+    props: { tabs, activeTabLabel: options?.activeTabLabel },
   });
-
-  // Because the tab content gets set in the `mount` hook, we need to await the
-  // next tick first to see the effect.
-  await wrapper.vm.$nextTick();
-
-  return wrapper;
 }
 
 describe('Tabs.vue', () => {
-  it('renders the label of each tab', async () => {
+  it('renders the label of each tab', () => {
     const tabs = [{ label: 'foo' }, { label: 'bar' }, { label: 'baz' }];
-    const wrapper = await createWrapper({ tabs });
+    const wrapper = createWrapper({ tabs });
     const tabHeaders = wrapper.findAll('[data-test="tab-header"]');
 
     expect(tabHeaders.length).toBe(3);
@@ -34,17 +31,25 @@ describe('Tabs.vue', () => {
     expect(tabHeaders[2].text()).toContain('baz');
   });
 
-  it('shows first tab content per default', async () => {
+  it('shows first tab content per default', () => {
     const tabs = [{ template: 'foo' }, { template: 'bar' }, { template: 'baz' }];
-    const wrapper = await createWrapper({ tabs });
+    const wrapper = createWrapper({ tabs });
     const tabContent = wrapper.get('[data-test="tab-content"]');
 
     expect(tabContent.html()).toContain('foo');
   });
 
+  it('can alternate first open tab via property', () => {
+    const tabs = [{ template: 'foo' }, { label: 'bar', template: 'bar' }];
+    const wrapper = createWrapper({ tabs, activeTabLabel: 'bar' });
+    const tabContent = wrapper.get('[data-test="tab-content"]');
+
+    expect(tabContent.html()).toContain('bar');
+  });
+
   it('can switch tab content via clicks on the headers', async () => {
     const tabs = [{ template: 'foo' }, { template: 'bar' }, { template: 'baz' }];
-    const wrapper = await createWrapper({ tabs });
+    const wrapper = createWrapper({ tabs });
     const tabHeaders = wrapper.findAll('[data-test="tab-header"]');
     const tabContent = wrapper.get('[data-test="tab-content"]');
 
@@ -59,9 +64,43 @@ describe('Tabs.vue', () => {
     expect(tabContent.html()).toContain('foo');
   });
 
+  it('can switch tab content via alternating the related property', async () => {
+    const tabs = [
+      { label: 'foo', template: 'foo' },
+      { label: 'bar', template: 'bar' },
+      { label: 'baz', template: 'baz' },
+    ];
+    const wrapper = createWrapper({ tabs });
+    const tabContent = wrapper.get('[data-test="tab-content"]');
+
+    expect(tabContent.html()).toContain('foo');
+    await wrapper.setProps({ ...wrapper.props, activeTabLabel: 'bar' });
+    expect(tabContent.html()).toContain('bar');
+    await wrapper.setProps({ ...wrapper.props, activeTabLabel: 'baz' });
+    expect(tabContent.html()).toContain('baz');
+    await wrapper.setProps({ ...wrapper.props, activeTabLabel: 'bar' });
+    expect(tabContent.html()).toContain('bar');
+    await wrapper.setProps({ ...wrapper.props, activeTabLabel: 'foo' });
+    expect(tabContent.html()).toContain('foo');
+  });
+
+  it('does nothing if trying to switch to unknown tab', async () => {
+    const tabs = [
+      { label: 'foo', template: 'foo' },
+      { label: 'bar', template: 'bar' },
+    ];
+    const wrapper = createWrapper({ tabs });
+    const tabContent = wrapper.get('[data-test="tab-content"]');
+    expect(tabContent.html()).toContain('foo');
+
+    await wrapper.setProps({ ...wrapper.props, activeTabLabel: 'baz' });
+
+    expect(tabContent.html()).toContain('foo');
+  });
+
   it('does not unload content component when tab changes', async () => {
     const tabs = [{ template: '<input id="test-input" />' }, { template: 'bar' }];
-    const wrapper = await createWrapper({ tabs });
+    const wrapper = createWrapper({ tabs });
     const tabHeaders = wrapper.findAll('[data-test="tab-header"]');
 
     // Just a stupid helper to get around typing issues.
