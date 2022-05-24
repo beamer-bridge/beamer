cb44532fae65b1ae1570da86a6c9a31696692759
Ivan Stanković <ivan@brainbot.com>
2022-05-24 09:34:28 +0200

    contracts: replace TestCrossDomainMessenger with TestProofSubmitter

---
+++ contracts/contracts/chains/test/TestProofSubmitter.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import "../../../interfaces/IMessenger.sol";
+import "../../../interfaces/IProofSubmitter.sol";
+
+import "../../BeamerUtils.sol";
+import "../../Resolver.sol";
+import "../../RestrictedCalls.sol";
+
+contract TestProofSubmitter is IProofSubmitter, RestrictedCalls {
+    IMessenger private messenger;
+    uint32 private constant MESSAGE_GAS_LIMIT = 1_000_000;
+
+    constructor(address messenger_) {
+        messenger = IMessenger(messenger_);
+    }
+
+    function submitProof(
+        address l1Resolver,
+        uint256 sourceChainId,
+        bytes32 requestHash,
+        address filler
+    )
+        external
+        restricted(block.chainid, msg.sender)
+        returns (ProofReceipt memory)
+    {
+        bytes32 fillId = blockhash(block.number - 1);
+        bytes32 fillHash = BeamerUtils.createFillHash(requestHash, fillId);
+
+        messenger.sendMessage(
+            l1Resolver,
+            abi.encodeCall(
+                Resolver.resolve,
+                (fillHash, block.chainid, sourceChainId, filler)
+            ),
+            MESSAGE_GAS_LIMIT
+        );
+
+        return ProofReceipt(fillId, fillHash);
+    }
+
+    function submitNonFillProof(
+        address l1Resolver,
+        uint256 sourceChainId,
+        bytes32 fillHash
+    ) external {
+        messenger.sendMessage(
+            l1Resolver,
+            abi.encodeCall(
+                Resolver.resolveNonFill,
+                (fillHash, block.chainid, sourceChainId)
+            ),
+            MESSAGE_GAS_LIMIT
+        );
+    }
+}
