0bc4ca63ed1525c6b9ba7d0564832386c8211ab6
Thore Strassburg <thore@weilbier.net>
2022-05-29 02:32:59 +0200

    frontend: recover new transfer button
    It has been requested to add (again) a "new transfer" button when the users
    views the activity tab. The navigation works as the transition from the request
    dialog to the activity tab.

--- frontend/src/router/navigation.ts
+++ frontend/src/router/navigation.ts
@@ -1,5 +1,9 @@
 import router from '@/router';
 
+export function switchToRequestDialog() {
+  router.replace({ path: '/', query: { activeTabLabel: 'Transfer' } });
+}
+
 export function switchToActivities() {
   router.replace({ path: '/', query: { activeTabLabel: 'Activity' } });
 }
