f97f90f2641da095d7695a41f38387b107330656
Thore Strassburg <thore@weilbier.net>
2022-06-07 12:47:44 +0200

    frontend: adapt dependency free progress component
    The DaisyUI dependency can be a pita and we want to get rid of it. Therefore we
    need to add custom implementations for the occurrences where we use it.
    One of these places were the `Progress` and `ProgressStep` components. They used
    the classes of `steps*` to get the list with circles and lines between.
    Recently, when we added error message to steps, it was not possible to create
    dynamic sizing of the steps. Furthermore we had to trick around with absolute
    placement, which lead to accidentally cut text etc. pp. Everything but not
    optimal. At this point a dependency is no more helpful but painful.
    This commit provides a custom implementation of these steps. The advantage: it
    is much less code than before and 100% customizable to our needs. So got the
    previously mentioned problems already solved. Any future request by the designer
    can be easily addressed too.
    
    Note that this commit merges two commits into one. First of all, it could be
    argued that the former split was simply overload. Second it is for the reason
    that it makes no sense to have all this special styling of the list items in an
    individual list item component. This only makes list for a whole list and
    therefore binding this together makes sense. Less code, less tests, better
    readability, 100% customizability.

--- frontend/src/components/layout/Progress.vue
+++ frontend/src/components/layout/Progress.vue
@@ -1,11 +1,19 @@
 <template>
-  <ul class="steps steps-vertical text-left w-full px-5">
-    <ProgressStep v-for="(step, index) in steps" :key="index" v-bind="step" />
+  <ul>
+    <li
+      v-for="step in steps"
+      :key="step.label"
+      :class="{ completed: step.completed, failed: step.failed, hasError: step.errorMessage }"
+    >
+      {{ step.label }}
+      <WaitingDots v-if="step.active" />
+      <span v-if="step.errorMessage" class="text-red"> <br />{{ step.errorMessage }} </span>
+    </li>
   </ul>
 </template>
 
 <script setup lang="ts">
-import ProgressStep from '@/components/layout/ProgressStep.vue';
+import WaitingDots from '@/components/layout/WaitingDots.vue';
 
 interface Props {
   readonly steps: Array<{
@@ -19,3 +27,39 @@ interface Props {
 
 defineProps<Props>();
 </script>
+
+<style>
+/* 
+Please note that the used numeric values here are based on `rem` to scale with
+the application, but are very fixed an not variable. If any of these value gets
+touched, it is likely to break somewhere else. Using variables and calculation
+only barely helps with this problem. As this is a very specialized custom
+component, this inflexibility can be tolerated for now.
+*/
+
+li {
+  @apply relative my-5 pl-10;
+}
+
+li::before {
+  content: '';
+  @apply absolute left-0 rounded-full w-7 h-7 text-center border-[2px] border-teal;
+}
+
+li.completed::before {
+  @apply bg-green;
+}
+
+li.failed::before {
+  @apply bg-red;
+}
+
+li:not(:last-of-type)::after {
+  content: '';
+  @apply absolute left-[0.8rem] border-teal border-[1px] top-[1.7rem] h-[1.45rem];
+}
+
+li.hasError::after {
+  @apply h-[3.2rem];
+}
+</style>
