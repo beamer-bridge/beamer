b7e4c3a1ba2433415eca2cd0d606b92aed749cb3
Paul Lange <palango@gmx.de>
2022-06-14 14:44:05 +0200

    agent: Use actual finalization times from `RequestManager`

--- beamer/util.py
+++ beamer/util.py
@@ -13,9 +13,6 @@ from web3.types import TxParams
 from beamer.typing import ChainId, ChecksumAddress
 
 
-SECONDS_PER_DAY = 24 * 60 * 60
-
-
 class TransactionFailed(Exception):
     def __repr__(self) -> str:
         return "TransactionFailed(%r)" % self.__cause__
