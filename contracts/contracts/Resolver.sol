d3d356c31f67d8b635f25e93107d1c25b0b33c53
Ivan Stanković <ivan@brainbot.com>
2022-06-23 12:47:03 +0200

    contracts: document Resolver

--- contracts/contracts/Resolver.sol
+++ contracts/contracts/Resolver.sol
@@ -6,12 +6,20 @@ import "../interfaces/IMessenger.sol";
 import "./ResolutionRegistry.sol";
 import "./CrossDomainRestrictedCalls.sol";
 
+/// The resolver.
+///
+/// This contract resides on the L1 chain and is tasked with receiving the
+/// fill or non-fill proofs from the target L2 chain and forwarding them to
+/// the :sol:contract:`ResolutionRegistry` on the source L2 chain.
 contract Resolver is Ownable, CrossDomainRestrictedCalls {
     struct ResolutionInfos {
         address resolutionRegistry;
         address messenger;
     }
 
+    /// Emitted when a fill or a non-fill proof is received and sent to the resolution registry.
+    ///
+    /// .. note:: In case of a non-fill proof, the ``filler`` will be zero.
     event Resolution(
         uint256 sourceChainId,
         uint256 fillChainId,
@@ -20,8 +28,26 @@ contract Resolver is Ownable, CrossDomainRestrictedCalls {
         bytes32 fillId
     );
 
+    /// Maps source chain IDs to resolution infos.
     mapping(uint256 => ResolutionInfos) public resolutionInfos;
 
+    /// Resolve the specified request.
+    ///
+    /// This marks the request identified by ``requestHash`` as filled by ``filler``.
+    /// Information about the fill will be sent to the source chain's :sol:contract:`ResolutionRegistry`,
+    /// using the messenger responsible for the source chain.
+    ///
+    /// .. note::
+    ///
+    ///     This function is callable only by the native L1 messenger contract,
+    ///     which simply delivers the message sent from the target chain by the
+    ///     Beamer's L2 :sol:interface:`messenger <IMessenger>` contract.
+    ///
+    /// @param requestHash The request hash.
+    /// @param fillId The fill ID.
+    /// @param fillChainId The fill (target) chain ID.
+    /// @param sourceChainId The source chain ID.
+    /// @param filler The address that filled the request.
     function resolve(
         bytes32 requestHash,
         bytes32 fillId,
@@ -58,6 +84,23 @@ contract Resolver is Ownable, CrossDomainRestrictedCalls {
         );
     }
 
+    /// Mark the fill as invalid.
+    ///
+    /// Callable only by the proof submitter contract via a messenger, this
+    /// marks the fill as invalid. Information about the invalid fill will be
+    /// sent to the source chain's :sol:contract:`ResolutionRegistry`,
+    /// using the messenger responsible for transferring messages to the source chain.
+    ///
+    /// .. note::
+    ///
+    ///     This function is callable only by the native L1 messenger contract,
+    ///     which simply delivers the message sent from the target chain by the
+    ///     Beamer's L2 :sol:interface:`messenger <IMessenger>` contract.
+    ///
+    /// @param requestHash The request hash.
+    /// @param fillId The fill ID.
+    /// @param fillChainId The fill (target) chain ID.
+    /// @param sourceChainId The source chain ID.
     function resolveNonFill(
         bytes32 requestHash,
         bytes32 fillId,
@@ -93,6 +136,19 @@ contract Resolver is Ownable, CrossDomainRestrictedCalls {
         );
     }
 
+    /// Add a resolution registry.
+    ///
+    /// In order to be able to send messages to the :sol:contract:`ResolutionRegistry`,
+    /// the resolver contract needs to know the address of the registry on the source
+    /// chain, as well as the address of the messenger contract responsible for
+    /// transferring messages to the L2 chain.
+    ///
+    /// .. note:: This function can only be called by the contract owner.
+    ///
+    /// @param chainId The source L2 chain ID.
+    /// @param resolutionRegistry The resolution registry.
+    /// @param messenger The messenger contract responsible for chain ``chainId``.
+    ///                  Must implement :sol:interface:`IMessenger`.
     function addRegistry(
         uint256 chainId,
         address resolutionRegistry,
