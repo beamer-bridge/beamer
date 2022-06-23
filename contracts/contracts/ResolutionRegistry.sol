3c41ceefb2929a4c9417832caabe692c3adf4433
Ivan Stanković <ivan@brainbot.com>
2022-06-23 11:15:18 +0200

    contracts: document ResolutionRegistry

--- contracts/contracts/ResolutionRegistry.sol
+++ contracts/contracts/ResolutionRegistry.sol
@@ -4,16 +4,46 @@ pragma solidity ^0.8.12;
 import "./CrossDomainRestrictedCalls.sol";
 import "./BeamerUtils.sol";
 
+/// The resolution registry.
+///
+/// This contract resides on the source L2 chain and is tasked with keeping track of results
+/// of L1 resolution. In particular, it stores the information about known fills and fillers,
+/// as well as fills that were marked invalid. This information is used by the :sol:contract:`RequestManager`
+/// to resolve claims.
+///
+/// .. note::
+///
+///   This contract can only be called by the :sol:contract:`Resolver` contract, via a
+///   chain-dependent messenger contract.
 contract ResolutionRegistry is CrossDomainRestrictedCalls {
+    /// Emitted when a request has been resolved via L1 resolution.
+    ///
+    /// .. seealso:: :sol:func:`resolveRequest`
     event RequestResolved(bytes32 requestHash, address filler, bytes32 fillId);
 
+    /// Emitted when a fill hash has been invalidated.
+    ///
+    /// .. seealso:: :sol:func:`invalidateFillHash`
     event FillHashInvalidated(bytes32 fillHash);
 
-    // mapping from requestHash to FillInfo
+    /// Maps request hashes to fill infos.
     mapping(bytes32 => BeamerUtils.FillInfo) public fillers;
-    // mapping from fillHash to validity flag
+
+    /// The set of invalid fill hashes.
     mapping(bytes32 => bool) public invalidFillHashes;
 
+    /// Mark the request identified by ``requestHash`` as filled by ``filler``.
+    ///
+    /// .. note::
+    ///
+    ///     This function is callable only by the native L2 messenger contract,
+    ///     which simply delivers the message sent from L1 by the
+    ///     Beamer's L2 :sol:interface:`messenger <IMessenger>` contract.
+    ///
+    /// @param requestHash The request hash.
+    /// @param fillId The fill ID.
+    /// @param resolutionChainId The resolution (L1) chain ID.
+    /// @param filler The address that filled the request.
     function resolveRequest(
         bytes32 requestHash,
         bytes32 fillId,
@@ -32,6 +62,17 @@ contract ResolutionRegistry is CrossDomainRestrictedCalls {
         emit RequestResolved(requestHash, filler, fillId);
     }
 
+    /// Mark the fill identified by ``fillId`` as invalid.
+    ///
+    /// .. note::
+    ///
+    ///     This function is callable only by the native L2 messenger contract,
+    ///     which simply delivers the message sent from L1 by the
+    ///     Beamer's L2 :sol:interface:`messenger <IMessenger>` contract.
+    ///
+    /// @param requestHash The request hash.
+    /// @param fillId The fill ID.
+    /// @param resolutionChainId The resolution (L1) chain ID.
     function invalidateFillHash(
         bytes32 requestHash,
         bytes32 fillId,
