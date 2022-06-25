793c300ce8840951c3eab1fe5e16d3f411871c68
Frederik Lührs <luehrs.fred@gmail.com>
2022-06-25 12:11:57 +0200

    agent: adapt poll frequency on RPC endpoints depending on test- or mainnet

--- beamer/agent.py
+++ beamer/agent.py
@@ -5,7 +5,12 @@ import structlog
 from eth_typing import Address
 
 import beamer.metrics
-from beamer.chain import EventMonitor, EventProcessor
+from beamer.chain import (
+    POLL_FREQUENCY_MAINNET,
+    POLL_FREQUENCY_TESTNET,
+    EventMonitor,
+    EventProcessor,
+)
 from beamer.config import Config
 from beamer.contracts import make_contracts
 from beamer.state_machine import Context
@@ -59,6 +64,10 @@ class Agent:
             task_pool=self._task_pool,
             l1_resolutions={},
         )
+
+        chain_id = w3_l1.eth.chain_id
+        poll_frequency = POLL_FREQUENCY_MAINNET if chain_id == 1 else POLL_FREQUENCY_TESTNET
+
         self._event_processor = EventProcessor(self.context)
 
         self._event_monitor_l2a = EventMonitor(
@@ -70,6 +79,7 @@ class Agent:
             ),
             on_new_events=self._event_processor.add_events,
             on_sync_done=self._event_processor.mark_sync_done,
+            poll_frequency=poll_frequency,
         )
 
         self._event_monitor_l2b = EventMonitor(
@@ -78,6 +88,7 @@ class Agent:
             deployment_block=l2b_contracts_info["FillManager"].deployment_block,
             on_new_events=self._event_processor.add_events,
             on_sync_done=self._event_processor.mark_sync_done,
+            poll_frequency=poll_frequency,
         )
 
     def start(self) -> None:
