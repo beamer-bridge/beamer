de9fbca3b8c9f115b11ded7da4ac4a6d024fa4ad
Thore Strassburg <thore@weilbier.net>
2022-07-04 11:12:53 +0200

    dapp: add option to disable faucet
    This feature is necessary for later production deployments where no faucet is
    available anyway. With this option set, it will also hide the faucet button at
    all. The option is strict and only enables if explicitly set. Any missing or
    unknown configuration will disable the faucet. As environment variables (as used
    here) are pure strings, the parsing to a boolean is an unnecessary step and
    thereby gets skipped.
    
    As this is a build-time configuration and we still plan to maintain a testing
    version, a new `staging` mode got introduced. Thereby we have no `development`,
    `staging` and `production`. The faucet is enabled for the first to cases.
    While the `development` version uses a local chain, `staging` connects to test
    chains. This means that for the moment the runtime configuration file for
    `staging` and `production` are equal so far, but will diverge in future.

--- frontend/src/composables/useFaucet.ts
+++ frontend/src/composables/useFaucet.ts
@@ -9,18 +9,27 @@ export function useFaucet(
   signer: Ref<JsonRpcSigner | undefined>,
   chainId: Ref<number | undefined>,
 ) {
+  const faucetEnabled = computed(() => import.meta.env.VITE_FAUCET_ENABLED === 'true');
   const faucetUsedForChain: Record<string, boolean> = reactive({});
   const faucetUsed = computed(() =>
     chainId.value ? faucetUsedForChain[chainId.value as unknown as number] : false,
   );
-  const available = computed(() => !faucetUsed.value && signer.value && chainId.value);
+  const available = computed(
+    () => faucetEnabled.value && !faucetUsed.value && signer.value && chainId.value,
+  );
 
   const executeFaucetRequest = async () => {
+    if (!faucetEnabled.value) {
+      throw new Error('Faucet is not enabled!');
+    }
+
     if (!signer.value || !chainId.value) {
       throw new Error('Signer or chain id missing!');
     }
+
     const signerAddress = await signer.value.getAddress();
     const isSuccessfulFaucetRequest = await requestFaucet(chainId.value, signerAddress);
+
     if (isSuccessfulFaucetRequest) {
       faucetUsedForChain[chainId.value] = true;
     }
--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,5 +1,6 @@
 interface ImportMetaEnv {
   readonly VITE_CONFIG_URL: string;
+  readonly VITE_FAUCET_ENABLED: string;
 }
 
 interface ImportMeta {
