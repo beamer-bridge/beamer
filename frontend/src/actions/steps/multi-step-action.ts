61dc2ac4bcbe564ee9732aba3666cb25e9076962
Thore Strassburg <thore@weilbier.net>
2022-07-07 11:32:48 +0200

    frontend: add inline documentation for core
    While we always aim to write self-explanatory code with coded assumptions and
    without much comments and external documentation, not all ideas/concepts can be
    expressed in code or it is inefficient. Therefore I added some details into the
    inline documentation of the most important core classes of the application.
    These are meant to include the business logic, implement the backwards
    compatibility etc. pp.
    I used these notes also in preparation for the code introduction calls with
    a new employee and adjusted it based on raised questions. These docs also
    include all the information that we put into the issues where this architecture
    got discussed.

--- frontend/src/actions/steps/multi-step-action.ts
+++ frontend/src/actions/steps/multi-step-action.ts
@@ -1,5 +1,45 @@
 import type { Step } from './step';
 
+/**
+ * A multi-step action aggregates multiple steps allows to execute them.
+ *
+ * An action acts as extension of steps to make them more usable in compounds.
+ * Therefore it provides convenient getters that aggregates metadata from all
+ * steps as metadata for the whole action.
+ *
+ * Finally the action allows to associate each step with an executable and run
+ * the whole action. Therefore it is important that the stored list of steps is
+ * strictly ordered. The provision of the executables happens on execution. The
+ * action ensures that there is an executable for each to run step.
+ *
+ * It is not possible to execute the same action twice in parallel or a second
+ * time. But it is possible to continue the execution of a step sequence at
+ * a later point in time. As there is no mechanism (yet) to actively stop the
+ * execution, this is only relevant when the whole application quits during the
+ * execution of the action. After reloading the encoded data of the action, it
+ * is possible to continue the execution from the last completed step on. Note
+ * that in this case it is not necessary to provide the executables of already
+ * completed steps.
+ * Thereby the association of steps data with their logic to execute always
+ * happens during runtime. In result actions do not provide any further
+ * guarantees of backwards compatibility by themselves, but allows to do so in
+ * a quite flexible manner by using actions. As an action can always become
+ * interrupted it is necessary to maintain executables for old steps that get
+ * loaded from the users storage. Though it is possible to adapt the actual
+ * internal implementation.
+ *
+ * The multi-step action class is meant to get extended as a specific action.
+ *
+ * As actions are intended to be preserved to storage and get reloaded, it must
+ * follow the rules of encodable data. Therefore an action MUST NOT save any
+ * references to executables as they can not be serialized. Furthermore this
+ * separates logic and data properly.
+ *
+ * Actions are an important part for backwards compatibility for the core
+ * business logic of the application. Thereby they MUST ALWAYS be optionally
+ * extended and NEVER introduce breaking changes to itself. Tests are an approach
+ * to ensure this.
+ */
 export class MultiStepAction {
   readonly _steps: Array<Step>;
 
