1d4d103a734ab1378461bdaf72f7f3383b404d77
manuelwedler <manuel.wedler@googlemail.com>
2022-07-21 15:12:03 +0200

    frontend: Fix typo in Imprint

--- frontend/src/components/ImprintModal.vue
+++ frontend/src/components/ImprintModal.vue
@@ -71,8 +71,8 @@
         <p>
           Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten
           möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift
-          oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger
-          Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte
+          oder E-Mail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf
+          freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte
           weitergegeben. Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der
           Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der
           Daten vor dem Zugriff durch Dritte ist nicht möglich. Der Nutzung von im Rahmen der
