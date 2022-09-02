08517a985a5f3fdc6c5946af063b8e5d220b24d0
GabrielBuragev <gabriel.buragev96@gmail.com>
2022-09-02 16:00:24 +0200

    frontend: fix incorrect positioned box-shadow

--- frontend/src/components/TransferHistory.vue
+++ frontend/src/components/TransferHistory.vue
@@ -1,41 +1,43 @@
 <template>
-  <div class="relative w-full h-full pt-3" v-bind="$attrs">
-    <!-- Top gradient to create phase out effect for scrolling content. -->
-    <div
-      class="absolute top-0 right-0 w-full h-10 z-10 bg-gradient-to-t from-transparent to-teal"
-    />
-
-    <div
-      ref="listElement"
-      class="w-full h-full overflow-y-auto overflow-hidden pb-20 no-scrollbar"
-    >
+  <div class="relative w-full h-full pt-6" v-bind="$attrs">
+    <div class="gradient-wrapper relative w-full h-full">
+      <!-- Top gradient to create phase out effect for scrolling content. -->
       <div
-        v-if="transfers.length === 0"
-        class="text-2xl w-full h-full flex justify-center items-center text-sea-green/40"
+        class="absolute top-0 right-0 w-full h-3 z-10 bg-gradient-to-t from-transparent to-teal"
+      />
+
+      <div
+        ref="listElement"
+        class="w-full h-full overflow-y-auto overflow-hidden pb-20 no-scrollbar"
       >
-        <span>Nothing here yet.</span>
+        <div
+          v-if="transfers.length === 0"
+          class="text-2xl w-full h-full flex justify-center items-center text-sea-green/40"
+        >
+          <span>Nothing here yet.</span>
+        </div>
+
+        <template v-for="group of groupedAndSortedTransfers" :key="group.label">
+          <LazyWrapper :root-element="listElement" :threshold="0.0">
+            <div v-if="group.transfers.length > 0" class="text-2xl text-center">
+              {{ group.label }}
+            </div>
+          </LazyWrapper>
+          <LazyWrapper
+            v-for="(transfer, groupTransferIndex) of group.transfers"
+            :key="transfer.requestInformation?.identifier?.asString ?? groupTransferIndex"
+            :threshold="0.0"
+            :root-element="listElement"
+          >
+            <TransferComponent :transfer="transfer" class="my-3" data-test="transfer" />
+          </LazyWrapper>
+          <div v-if="group.transfers.length > 0" class="h-12"></div>
+        </template>
       </div>
 
-      <template v-for="group of groupedAndSortedTransfers" :key="group.label">
-        <LazyWrapper :root-element="listElement" :threshold="0.0">
-          <div v-if="group.transfers.length > 0" class="text-2xl text-center">
-            {{ group.label }}
-          </div>
-        </LazyWrapper>
-        <LazyWrapper
-          v-for="(transfer, groupTransferIndex) of group.transfers"
-          :key="transfer.requestInformation?.identifier?.asString ?? groupTransferIndex"
-          :threshold="0.0"
-          :root-element="listElement"
-        >
-          <TransferComponent :transfer="transfer" class="my-3" data-test="transfer" />
-        </LazyWrapper>
-        <div v-if="group.transfers.length > 0" class="h-12"></div>
-      </template>
+      <!-- Bottom gradient to create phase out effect for scrolling content. -->
+      <div class="absolute bottom-0 right-0 w-full h-3 z-10 bg-gradient-to-t from-teal" />
     </div>
-
-    <!-- Bottom gradient to create phase out effect for scrolling content. -->
-    <div class="absolute bottom-0 right-0 w-full h-10 z-10 bg-gradient-to-t from-teal" />
   </div>
 
   <SafeTeleport to="#action-button-portal">
