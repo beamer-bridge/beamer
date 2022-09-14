732396409278062e5c66f6052407b35f59c9aca3
Frederik Lührs <luehrs.fred@gmail.com>
2022-09-14 13:32:31 +0200

    chore: add new constants indices to access filler and fillid

--- beamer/tests/constants.py
+++ beamer/tests/constants.py
@@ -5,8 +5,10 @@ FILL_ID = FillId("abc".zfill(64))
 FILL_ID_EMPTY = FillId(bytes(32))
 
 # request fields
-RM_R_FIELD_LP_FEE = 7
-RM_R_FIELD_PROTOCOL_FEE = 8
+RM_R_FIELD_LP_FEE = 5
+RM_R_FIELD_PROTOCOL_FEE = 6
+RM_R_FIELD_FILLER = 9
+RM_R_FIELD_FILL_ID = 10
 
 # claim fiels
 RM_C_FIELD_TERMINATION = 6
