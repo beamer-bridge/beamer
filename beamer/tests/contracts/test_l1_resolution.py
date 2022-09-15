84021e5d717039a19080da0179f2b32587d12b23
Frederik Lührs <luehrs.fred@gmail.com>
2022-09-15 14:32:02 +0200

    tests: move test test_l1_resolution and remove test_resolution_registry.py

--- beamer/tests/contracts/test_l1_resolution.py
+++ beamer/tests/contracts/test_l1_resolution.py
@@ -1,10 +1,13 @@
 import brownie
 import pytest
+from brownie import web3
 from brownie.convert.datatypes import HexString
 from web3.constants import ADDRESS_ZERO
 
-from beamer.tests.constants import FILL_ID_EMPTY, RM_R_FIELD_FILL_ID, RM_R_FIELD_FILLER
+from beamer.tests.agent.utils import make_address
+from beamer.tests.constants import FILL_ID, FILL_ID_EMPTY, RM_R_FIELD_FILL_ID, RM_R_FIELD_FILLER
 from beamer.tests.util import alloc_accounts, alloc_whitelisted_accounts, create_request_id
+from beamer.typing import RequestId
 
 
 @pytest.mark.parametrize("amount", [100, 99, 101])
@@ -54,6 +57,36 @@ def test_l1_non_fill_proof(fill_manager, request_manager):
 
     fill_manager.invalidateFill(request_id, fill_id, chain_id)
     assert request_manager.isInvalidFill(request_id, fill_id)
+
+
+def test_invalidation_before_and_after_resolution(contracts, request_manager):
+    contracts.l1_messenger.setLastSender(contracts.resolver.address)
+
+    address = make_address()
+    request_id = RequestId(31 * b"0" + b"1")
+    chain_id = web3.eth.chain_id
+
+    assert not request_manager.isInvalidFill(request_id, FILL_ID)
+    request_manager.invalidateFill(request_id, FILL_ID, chain_id, {"from": contracts.l1_messenger})
+
+    # Fill must be invalidated
+    assert request_manager.isInvalidFill(request_id, FILL_ID)
+
+    assert request_manager.requests(request_id)[RM_R_FIELD_FILLER] == ADDRESS_ZERO
+
+    request_manager.resolveRequest(
+        request_id, FILL_ID, chain_id, address, {"from": contracts.l1_messenger}
+    )
+
+    # Resolution validates fill again
+    assert not request_manager.isInvalidFill(request_id, FILL_ID)
+    assert request_manager.requests(request_id)[RM_R_FIELD_FILLER] == address
+
+    # Invalidation of a resolved request should fail
+    with brownie.reverts("Cannot invalidate resolved fills"):
+        request_manager.invalidateFill(
+            request_id, FILL_ID, chain_id, {"from": contracts.l1_messenger}
+        )
 
 
 def test_restricted_calls(contracts, resolver, request_manager):
