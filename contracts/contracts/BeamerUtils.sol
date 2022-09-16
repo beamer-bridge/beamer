5a89a176ba7e3d2ad773f4ce600dff3aa426a19e
Frederik Lührs <luehrs.fred@gmail.com>
2022-09-16 09:27:39 +0200

    contracts: Remove createFillHash functions and FillInfo from BeamerUtils

--- contracts/contracts/BeamerUtils.sol
+++ contracts/contracts/BeamerUtils.sol
@@ -2,11 +2,6 @@
 pragma solidity ^0.8.12;
 
 library BeamerUtils {
-    struct FillInfo {
-        address filler;
-        bytes32 fillId;
-    }
-
     function createRequestId(
         uint256 sourceChainId,
         uint256 targetChainId,
@@ -25,37 +20,6 @@ library BeamerUtils {
                     amount,
                     nonce
                 )
-            );
-    }
-
-    function createFillHash(bytes32 requestId, bytes32 fillId)
-        internal
-        pure
-        returns (bytes32)
-    {
-        return keccak256(abi.encodePacked(requestId, fillId));
-    }
-
-    function createFillHash(
-        uint256 sourceChainId,
-        uint256 targetChainId,
-        address targetTokenAddress,
-        address targetReceiverAddress,
-        uint256 amount,
-        uint256 nonce,
-        bytes32 fillId
-    ) internal pure returns (bytes32) {
-        return
-            createFillHash(
-                createRequestId(
-                    sourceChainId,
-                    targetChainId,
-                    targetTokenAddress,
-                    targetReceiverAddress,
-                    amount,
-                    nonce
-                ),
-                fillId
             );
     }
 }
