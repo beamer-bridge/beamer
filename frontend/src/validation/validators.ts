e328e7b3bf006a2a4f829fbd7f6771bbefc9f050
GabrielBuragev <gabriel.buragev96@gmail.com>
2022-09-19 12:00:24 +0200

    frontend: add blacklist target address validation
    Blacklisted wallet addresses should have limited access to our dApp.
    Transfers should be prohibited when target address is detected
    to be a part of the blacklist.
    The address blacklist is going to be manually maintained for now.

--- frontend/src/validation/validators.ts
+++ frontend/src/validation/validators.ts
@@ -1,12 +1,13 @@
-import { ethers } from 'ethers';
+import { isAddress } from 'ethers/lib/utils';
 
-import type { Chain } from '@/types/data';
+import type { Chain, EthereumAddress } from '@/types/data';
 import type { TokenAmount } from '@/types/token-amount';
+import { isAddressBlacklisted } from '@/utils/addressBlacklist';
 
 type ValidatorFunction<T> = (value: T) => boolean;
 
 export const isValidEthAddress: ValidatorFunction<string> = function (value: string): boolean {
-  return ethers.utils.isAddress(value);
+  return isAddress(value);
 };
 
 export const isUnsignedNumeric: ValidatorFunction<string> = function (value: string): boolean {
@@ -37,10 +38,17 @@ export const makeMaxTokenAmountValidator =
     return max.uint256.gte(value.uint256);
   };
 
+export const makeIsNotBlacklistedEthAddressValidator =
+  (blacklist?: EthereumAddress[]): ValidatorFunction<EthereumAddress> =>
+  (value: EthereumAddress): boolean => {
+    return !isAddressBlacklisted(value, blacklist);
+  };
+
 export default {
   isValidEthAddress,
   makeMatchingDecimalsValidator,
   makeNotSameAsChainValidator,
   makeMinTokenAmountValidator,
   makeMaxTokenAmountValidator,
+  makeIsNotBlacklistedEthAddressValidator,
 };
