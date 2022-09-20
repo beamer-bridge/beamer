1a04f0be64c02347cddaa91ebe2f133704351e3c
GabrielBuragev <gabriel.buragev96@gmail.com>
2022-09-20 22:40:56 +0200

    frontend: integrate chatbot

--- frontend/src/components/Feedback.vue
+++ frontend/src/components/Feedback.vue
@@ -1,12 +1,21 @@
 <template>
   <div
-    data-tf-popover="cW7uCBNz"
-    data-tf-custom-icon="https://images.typeform.com/images/B5etiKdwmjaS"
-    data-tf-button-color="#005E63"
-    data-tf-tooltip="Hey 👋&nbsp;&nbsp;How was your experience?"
+    data-tf-popover="DjKClKBT"
+    data-tf-custom-icon="https://images.typeform.com/images/KN5E4QHWSekY"
+    data-tf-button-color="#FFFFFF"
+    data-tf-notification-days="3"
+    data-tf-tooltip="Hey 👋&nbsp;&nbsp;Let us know what you think!"
+    data-tf-open="scroll"
+    data-tf-open-value="30"
     data-tf-chat
     data-tf-medium="snippet"
-    data-tf-hidden="utm_source=bridgeui,utm_medium=chatbot,utm_campaign=testfeedback,utm_content=form"
+    data-tf-hidden="utm_source=xxxxx,utm_medium=xxxxx,utm_campaign=xxxxx,utm_content=xxxxx"
     style="all: unset"
   ></div>
 </template>
+
+<style>
+.tf-v1-popover-button {
+  @apply drop-shadow-xl hover:scale-110 transition-all;
+}
+</style>
