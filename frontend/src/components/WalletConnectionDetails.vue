b9deb335ae613392bf339472b16c928a945ff7f5
GabrielBuragev <gabriel.buragev96@gmail.com>
2022-09-22 21:50:57 +0200

    frontend: create wallet connection error component
    There was an opportunity for refactoring the Home view once again
    by moving the wallet connection error into its own highly reusable
    component.
    Moving small chunks of code from the home view to its own components
    will make the process of writing integration/unit tests way easier.

--- frontend/src/components/WalletConnectionDetails.vue
+++ frontend/src/components/WalletConnectionDetails.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="h-14">
+  <div class="h-28 flex flex-col justify-center items-center">
+    <ConnectionError v-slot="{ message }" data-test="error-component">
+      <div class="text-center text-red p-2 text-lg h-12">
+        {{ message }}
+      </div>
+    </ConnectionError>
+
     <div v-if="signer" class="flex flex-row gap-4 justify-center items-center" data-test="details">
       <div class="h-7 w-7 rounded-50 border-[3px] border-solid border-sea-green bg-green"></div>
       <div class="flex flex-col w-full">
@@ -12,6 +18,7 @@
 <script lang="ts" setup>
 import { storeToRefs } from 'pinia';
 
+import ConnectionError from '@/components/wallet/ConnectionError.vue';
 import Disconnect from '@/components/wallet/Disconnect.vue';
 import { useEthereumProvider } from '@/stores/ethereum-provider';
 
