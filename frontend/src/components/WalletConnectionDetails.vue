a3f74d57716979d82b52e9d9924ee86e8430425d
GabrielBuragev <gabriel.buragev96@gmail.com>
2022-09-22 21:46:52 +0200

    frontend: add wallet disconnect feature

---
+++ frontend/src/components/WalletConnectionDetails.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="h-14">
+    <div v-if="signer" class="flex flex-row gap-4 justify-center items-center" data-test="details">
+      <div class="h-7 w-7 rounded-50 border-[3px] border-solid border-sea-green bg-green"></div>
+      <div class="flex flex-col w-full">
+        <span class="text-lg">You are currently connected</span>
+        <Disconnect> </Disconnect>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia';
+
+import Disconnect from '@/components/wallet/Disconnect.vue';
+import { useEthereumProvider } from '@/stores/ethereum-provider';
+
+const ethereumProvider = useEthereumProvider();
+const { signer } = storeToRefs(ethereumProvider);
+</script>
---
+++ frontend/tests/unit/components/WalletConnectionDetails.spec.ts
@@ -0,0 +1,53 @@
+import { mount } from '@vue/test-utils';
+import { ref } from 'vue';
+
+import Disconnect from '@/components/wallet/Disconnect.vue';
+import WalletConnectionDetails from '@/components/WalletConnectionDetails.vue';
+import * as useEthereumProviderComposable from '@/stores/ethereum-provider';
+
+vi.mock('@/stores/ethereum-provider');
+
+function createWrapper() {
+  return mount(WalletConnectionDetails, {
+    shallow: true,
+    global: {
+      stubs: {
+        ConnectionError: {
+          template: `<div><slot/></div>`,
+        },
+      },
+    },
+  });
+}
+
+describe('WalletConnectionDetails.vue', () => {
+  describe('when wallet provider is connected', () => {
+    beforeEach(() => {
+      Object.defineProperty(useEthereumProviderComposable, 'useEthereumProvider', {
+        value: vi.fn().mockReturnValue({
+          signer: ref('fake-signer'),
+        }),
+      });
+    });
+
+    it('renders wallet disconnect component', () => {
+      const wrapper = createWrapper();
+      const disconnectTrigger = wrapper.findComponent(Disconnect);
+      expect(disconnectTrigger.exists()).toBe(true);
+    });
+
+    it('shows successful connection status message', () => {
+      const wrapper = createWrapper();
+      const details = wrapper.find('[data-test="details"]');
+      expect(details.text()).toContain('You are currently connected');
+    });
+  });
+
+  describe('when wallet provider is not connected', () => {
+    it('hides connection details', () => {
+      const wrapper = createWrapper();
+      const details = wrapper.find('[data-test="details"]');
+      expect(details.exists()).toBe(false);
+    });
+  });
+});
