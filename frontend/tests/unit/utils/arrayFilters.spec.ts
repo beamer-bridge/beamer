c1a5c5ffd267fd5e55a5c6f69ce1167475ed4ecb
GabrielBuragev <gabriel.buragev96@gmail.com>
2022-10-07 15:46:47 +0200

    frontend: tests: arrayFilters util

---
+++ frontend/tests/unit/utils/arrayFilters.spec.ts
@@ -0,0 +1,33 @@
+import { removeMatchesByProperty } from '@/utils/arrayFilters';
+
+describe('filters', () => {
+  describe('removeMatchesByProperty()', () => {
+    it('filters out the objects from an array that are found in another array using property comparison', () => {
+      const arrayRemoveFrom = [{ id: 1 }, { id: 4 }];
+      const arrayCompareWith = [{ id: 4 }];
+      const comparisonProperty = 'id';
+
+      const result = removeMatchesByProperty(
+        arrayRemoveFrom,
+        arrayCompareWith,
+        comparisonProperty,
+      );
+
+      expect(result).toEqual([{ id: 1 }]);
+    });
+
+    it('objects that are not common in both arrays are left intact', () => {
+      const arrayRemoveFrom = [{ id: 1 }, { id: 4 }];
+      const arrayCompareWith = [{ id: 3 }];
+      const comparisonProperty = 'id';
+
+      const result = removeMatchesByProperty(
+        arrayRemoveFrom,
+        arrayCompareWith,
+        comparisonProperty,
+      );
+
+      expect(result).toEqual(arrayRemoveFrom);
+    });
+  });
+});
