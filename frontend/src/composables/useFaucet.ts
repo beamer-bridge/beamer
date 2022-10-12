6193859e8b83be6899955abc9a0ba4a0ba7e3c6c
GabrielBuragev <gabriel.buragev96@gmail.com>
2022-10-12 15:49:41 +0200

    frontend: tests: useFaucet composable
    Provided tests covering all the `useFaucet` composable functionalities
    as well as a small refactor.
    I found the passing of a `signer` as a param to be unnecessary since
    the composable only needs the wallet address used as a target address
    for directing the faucet funds to.
    There should be no need of a complete signer object since we dont use
    any of the signing functionalities of that object here.

--- frontend/src/composables/useFaucet.ts
+++ frontend/src/composables/useFaucet.ts
@@ -1,12 +1,12 @@
-import type { JsonRpcSigner } from '@ethersproject/providers';
 import type { Ref } from 'vue';
 import { computed, reactive } from 'vue';
 
 import { useAsynchronousTask } from '@/composables/useAsynchronousTask';
 import { requestFaucet } from '@/services/transactions/faucet';
+import type { EthereumAddress } from '@/types/data';
 
 export function useFaucet(
-  signer: Ref<JsonRpcSigner | undefined>,
+  ethereumAddress: Ref<EthereumAddress | undefined>,
   chainId: Ref<number | undefined>,
 ) {
   const enabled = computed(() => import.meta.env.VITE_FAUCET_ENABLED === 'true');
@@ -14,20 +14,24 @@ export function useFaucet(
   const faucetUsed = computed(() =>
     chainId.value ? faucetUsedForChain[chainId.value as unknown as number] : false,
   );
-  const available = computed(() => !faucetUsed.value && signer.value && chainId.value);
+  const available = computed(
+    () => !faucetUsed.value && !!ethereumAddress.value && !!chainId.value,
+  );
 
   const executeFaucetRequest = async () => {
     if (!enabled.value) {
       throw new Error('Faucet is not enabled!');
     }
 
-    if (!signer.value || !chainId.value) {
-      throw new Error('Signer or chain id missing!');
+    if (faucetUsed.value) {
+      throw new Error('Maximum allowed faucet requests exceeded!');
     }
 
-    const signerAddress = await signer.value.getAddress();
-    const isSuccessfulFaucetRequest = await requestFaucet(chainId.value, signerAddress);
+    if (!ethereumAddress.value || !chainId.value) {
+      throw new Error('Address or chain id missing!');
+    }
 
+    const isSuccessfulFaucetRequest = await requestFaucet(chainId.value, ethereumAddress.value);
     if (isSuccessfulFaucetRequest) {
       faucetUsedForChain[chainId.value] = true;
     }
