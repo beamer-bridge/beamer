da051b6ae77f098469e86826123fd8efbe613883
GabrielBuragev <gabriel.buragev96@gmail.com>
2022-10-13 18:51:50 +0200

    frontent: tests: useTokenBalance composable

---
+++ frontend/tests/unit/composables/useTokenBalance.spec.ts
@@ -0,0 +1,118 @@
+import { flushPromises } from '@vue/test-utils';
+import { ref, shallowRef } from 'vue';
+
+import { useTokenBalance } from '@/composables/useTokenBalance';
+import * as tokenService from '@/services/transactions/token';
+import { TokenAmount } from '@/types/token-amount';
+import { generateToken, getRandomEthereumAddress } from '~/utils/data_generators';
+import { MockedEthereumProvider } from '~/utils/mocks/ethereum-provider';
+
+vi.mock('@/services/transactions/token');
+
+const PROVIDER = shallowRef(
+  new MockedEthereumProvider({
+    signerAddress: getRandomEthereumAddress(),
+  }),
+);
+const TOKEN = ref(generateToken());
+
+describe('useTokenBalance', () => {
+  describe('balance', () => {
+    it('is undefined when provided ethereum provider is not defined', () => {
+      const provider = ref(undefined);
+
+      const { balance } = useTokenBalance(provider, TOKEN);
+
+      expect(balance.value).toBeUndefined();
+    });
+    it('is undefined when provided token is not defined', () => {
+      const token = ref(undefined);
+
+      const { balance } = useTokenBalance(PROVIDER, token);
+
+      expect(balance.value).toBeUndefined();
+    });
+    it('is undefined when provided ethereum provider lacks an ethereum address', () => {
+      const provider = shallowRef(new MockedEthereumProvider());
+
+      const { balance } = useTokenBalance(provider, TOKEN);
+
+      expect(balance.value).toBeUndefined();
+    });
+    it('is defined when all conditions are met for its calculation', async () => {
+      const token = ref(generateToken());
+      Object.defineProperty(tokenService, 'getTokenBalance', {
+        value: vi.fn().mockResolvedValue(TokenAmount.parse('1', token.value)),
+      });
+      const { balance } = useTokenBalance(PROVIDER, token);
+      await flushPromises();
+
+      expect(balance.value).not.toBeUndefined();
+      expect(balance.value?.token).toEqual(token.value);
+    });
+  });
+
+  describe('formattedBalance', () => {
+    it('holds the user token balance represented as a human readable text', async () => {
+      const token = ref(generateToken());
+
+      Object.defineProperty(tokenService, 'getTokenBalance', {
+        value: vi.fn().mockResolvedValue(TokenAmount.parse('1', token.value)),
+      });
+
+      const { formattedBalance } = useTokenBalance(PROVIDER, token);
+      await flushPromises();
+      expect(formattedBalance.value).not.toBeUndefined();
+      expect(formattedBalance.value).not.toHaveLength(0);
+    });
+  });
+
+  describe('error', () => {
+    it('is undefined when no errors occured', async () => {
+      const { error } = useTokenBalance(PROVIDER, TOKEN);
+      await flushPromises();
+      expect(error.value).toBeUndefined();
+    });
+
+    it('holds the rejected error of a failed token balance "fetch" operation', async () => {
+      Object.defineProperty(tokenService, 'getTokenBalance', {
+        value: vi.fn().mockImplementation(() => {
+          throw new Error('Failed fetching token balance');
+        }),
+      });
+
+      const { error } = useTokenBalance(PROVIDER, TOKEN);
+      await flushPromises();
+      expect(error.value).toBe('Failed fetching token balance');
+    });
+    it('holds the rejected error of a failed token balance "listener attach" operation', async () => {
+      Object.defineProperty(tokenService, 'listenOnTokenBalanceChange', {
+        value: vi.fn().mockImplementation(() => {
+          throw new Error('Failed attaching listeners on token balance');
+        }),
+      });
+
+      const { error } = useTokenBalance(PROVIDER, TOKEN);
+      await flushPromises();
+      expect(error.value).toBe('Failed attaching listeners on token balance');
+    });
+  });
+
+  it('detaches all contract listeners when parameters transition to invalid state', async () => {
+    const token = ref(generateToken());
+    const contractWithListeners = {
+      removeAllListeners: vi.fn(),
+    };
+
+    Object.defineProperty(tokenService, 'listenOnTokenBalanceChange', {
+      value: vi.fn().mockReturnValue(contractWithListeners),
+    });
+
+    useTokenBalance(PROVIDER, token);
+
+    token.value = generateToken();
+    await flushPromises();
+
+    expect(contractWithListeners.removeAllListeners).toHaveBeenCalledOnce();
+  });
+});
