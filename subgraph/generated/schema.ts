5f605ed97cce99470a9b9a4a01c17ec3bd615f83
Gabriel Buragev <gabriel.buragev96@gmail.com>
2022-11-03 11:16:46 +0100

    subgraph: remove boilerplate code

--- subgraph/generated/schema.ts
+++ subgraph/generated/schema.ts
@@ -11,65 +11,6 @@ import {
   BigDecimal
 } from "@graphprotocol/graph-ts";
 
-export class ExampleEntity extends Entity {
-  constructor(id: string) {
-    super();
-    this.set("id", Value.fromString(id));
-  }
-
-  save(): void {
-    let id = this.get("id");
-    assert(id != null, "Cannot save ExampleEntity entity without an ID");
-    if (id) {
-      assert(
-        id.kind == ValueKind.STRING,
-        `Entities of type ExampleEntity must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
-      );
-      store.set("ExampleEntity", id.toString(), this);
-    }
-  }
-
-  static load(id: string): ExampleEntity | null {
-    return changetype<ExampleEntity | null>(store.get("ExampleEntity", id));
-  }
-
-  get id(): string {
-    let value = this.get("id");
-    return value!.toString();
-  }
-
-  set id(value: string) {
-    this.set("id", Value.fromString(value));
-  }
-
-  get count(): BigInt {
-    let value = this.get("count");
-    return value!.toBigInt();
-  }
-
-  set count(value: BigInt) {
-    this.set("count", Value.fromBigInt(value));
-  }
-
-  get requestId(): BigInt {
-    let value = this.get("requestId");
-    return value!.toBigInt();
-  }
-
-  set requestId(value: BigInt) {
-    this.set("requestId", Value.fromBigInt(value));
-  }
-
-  get claimId(): BigInt {
-    let value = this.get("claimId");
-    return value!.toBigInt();
-  }
-
-  set claimId(value: BigInt) {
-    this.set("claimId", Value.fromBigInt(value));
-  }
-}
-
 export class HashInvalidated extends Entity {
   constructor(id: string) {
     super();
