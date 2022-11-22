5cb69f47a90a72cdad6241825cb09605984d0edc
GabrielBuragev <gabriel.buragev96@gmail.com>
2022-11-22 15:16:24 +0100

    frontend: add transfer-history versioning strategy

--- frontend/src/stores/transfer-history/store.ts
+++ frontend/src/stores/transfer-history/store.ts
@@ -5,7 +5,19 @@ import type { Transfer } from '@/actions/transfers';
 import { transferHistorySerializer } from './serializer';
 import type { TransferHistoryState } from './types';
 
-export const useTransferHistory = defineStore('transferHistory', {
+export function getAppMajorVersion(semVer: string): string {
+  return semVer.split('.')[0] || '0';
+}
+
+export function getStoreName(appVersion: string): string {
+  if (appVersion === '0') {
+    return 'transferHistory';
+  } else {
+    return `transferHistory_${appVersion}`;
+  }
+}
+
+export const useTransferHistory = defineStore(getStoreName(getAppMajorVersion(APP_VERSION)), {
   state: (): TransferHistoryState => ({
     transfers: [],
     loaded: false,
--- frontend/tests/unit/stores/transfer-history/store.spec.ts
+++ frontend/tests/unit/stores/transfer-history/store.spec.ts
@@ -1,7 +1,30 @@
 import { createPinia, setActivePinia } from 'pinia';
 
-import { useTransferHistory } from '@/stores/transfer-history/store';
+import {
+  getAppMajorVersion,
+  getStoreName,
+  useTransferHistory,
+} from '@/stores/transfer-history/store';
 import { generateTransfer } from '~/utils/data_generators';
+
+describe('store utility functions', () => {
+  describe('getAppMajorVersion()', () => {
+    it('returns the major version from the provided semver string', () => {
+      const semver = '1.5.3';
+      expect(getAppMajorVersion(semver)).toBe('1');
+    });
+  });
+  describe('getStoreName()', () => {
+    it('returns the base store name when app major version is 0', () => {
+      const storeName = getStoreName(getAppMajorVersion('0.1.0'));
+      expect(storeName).toBe('transferHistory');
+    });
+    it('returns the store name based on the app major version', () => {
+      const storeName = getStoreName(getAppMajorVersion('2.1.0'));
+      expect(storeName).toBe('transferHistory_2');
+    });
+  });
+});
 
 describe('configuration store', () => {
   beforeEach(() => {
