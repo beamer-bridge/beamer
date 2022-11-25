9880960fc6b4bf5c3ef7f5bf6f05bfcaa350d6e5
Gabriel Buragev <gabriel.buragev96@gmail.com>
2022-11-25 17:50:38 +0100

    frontend: tests: metamask-provider service

---
+++ frontend/src/services/web3-provider/util-export.ts
@@ -0,0 +1,13 @@
+/**
+ * In order to achieve testability of our external wallet-provider implementations
+ * we have to re-export some of their utility functions via named exports
+ */
+import detectEthereumProvider from '@metamask/detect-provider';
+import MetaMaskOnboarding from '@metamask/onboarding';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore TODO: Install & use type declarations once we migrate to WalletConnect v2
+import WalletConnectUMD from '@walletconnect/web3-provider/dist/umd/index.min.js';
+
+const WalletConnect = WalletConnectUMD.default;
+
+export { detectEthereumProvider, MetaMaskOnboarding, WalletConnect };
