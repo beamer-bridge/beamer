65cbbcfd011b37b5040066b6ab9f2fb565b1268f
Gabriel Buragev <gabriel.buragev96@gmail.com>
2022-12-21 14:06:10 +0100

    relayer: activate prettier plugin inside linter

--- relayer/.eslintrc.js
+++ relayer/.eslintrc.js
@@ -2,36 +2,23 @@ module.exports = {
   env: {
     browser: false,
     commonjs: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 12
+    ecmaVersion: 12,
   },
-  plugins: [
-    "@typescript-eslint",
-  ],
+  plugins: ["@typescript-eslint"],
   rules: {
-    indent: [
-      "error",
-      2
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    quotes: [
-      "error",
-      "double"
-    ],
-    semi: [
-      "error",
-      "always"
-    ],
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
     "@typescript-eslint/consistent-type-imports": "warn",
-  }
+  },
 };
