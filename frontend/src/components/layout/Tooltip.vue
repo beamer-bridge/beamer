04f171e69c9ff109be2a7267eabdb981cc9d5927
Daniel Dimitrov <daniel@compojoom.com>
2023-01-13 16:32:45 +0100

    frontend: fix by removing tooltip tests
    The tooltip arrow position is now no longer dependent on javascript calculations, but instead we’ve moved the centering of the arrow to css. Because of that I think that there is no longer need for those tests

--- frontend/src/components/layout/Tooltip.vue
+++ frontend/src/components/layout/Tooltip.vue
@@ -33,7 +33,6 @@ interface Props {
   referenceElementQuery?: string;
   tooltipWidth?: string;
   gap?: string;
-  arrowSize?: number; // in pixels
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -42,7 +41,6 @@ const props = withDefaults(defineProps<Props>(), {
   referenceElementQuery: '.tooltip-reference-element',
   tooltipWidth: '20rem',
   gap: '0.5rem',
-  arrowSize: 10,
 });
 
 const ZERO_BOUNDARIES = { top: 0, left: 0, bottom: 0, right: 0, width: 0, height: 0 };
@@ -119,31 +117,14 @@ const tooltipStyle = computed(() => {
 
 const arrowClasses = computed(() => {
   const defaultBottom =
-    'border-solid border-b-teal-500 border-b-8 border-x-transparent border-x-8 border-t-0 -top-2.5';
+    'border-solid border-b-teal-500 border-b-8 border-x-transparent border-x-8 border-t-0 -top-2 left-1/2';
   switch (position.value) {
     case 'right':
-      return `border-solid border-r-teal-500 border-r-8 border-y-transparent border-y-8 border-l-0 -right-5 -left-1.5`;
+      return `border-solid border-r-teal-500 border-r-8 border-y-transparent border-y-8 border-l-0 -left-1 top-1/2`;
 
     case 'bottom':
     default:
       return defaultBottom;
-  }
-});
-
-const arrowStyles = computed(() => {
-  const { arrowSize: size } = props;
-  const baseStyle = { width: `${size}px`, height: `${size}px` };
-  const alignOffset = `calc(50% - ${size / 0.7}px)`;
-
-  switch (position.value) {
-    case 'bottom':
-      return { ...baseStyle, right: alignOffset };
-
-    case 'right':
-      return { ...baseStyle, top: alignOffset };
-
-    default:
-      return baseStyle;
   }
 });
 
