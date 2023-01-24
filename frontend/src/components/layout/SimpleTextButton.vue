3fb7f19f97eaeec0d8a49b8db2daffb018df13ae
Gabriel Buragev <gabriel.buragev96@gmail.com>
2023-01-24 18:45:37 +0100

    frontend: fix dialog button positions
    Introduced a new `SimpleTextButton` component to wrap the stylings
    of the simple flat button in the request form.
    It was a clear duplication of code so i used the chance to clean this up

---
+++ frontend/src/components/layout/SimpleTextButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="text-orange text-xs font-semibold hover:bg-sea-green/20 px-2 rounded-md disabled:hover:bg-transparent disabled:opacity-25 disabled:text-grey"
+  >
+    <slot></slot>
+  </button>
+</template>
