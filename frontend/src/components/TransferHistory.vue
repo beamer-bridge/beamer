b9f70ce142d72d174d8c6b4ef5c43b36c541b70d
Gabriel Buragev <gabriel.buragev96@gmail.com>
2023-01-24 17:27:34 +0100

    frontend: fix transfer card flickering

--- frontend/src/components/TransferHistory.vue
+++ frontend/src/components/TransferHistory.vue
@@ -1,52 +1,42 @@
 <template>
   <div class="relative w-full h-full pt-6 rounded-b-lg overflow-hidden pb-5" v-bind="$attrs">
-    <div class="gradient-wrapper relative w-full h-full">
+    <div ref="listElement" class="w-full h-full overflow-y-auto overflow-hidden no-scrollbar">
       <div
-        ref="listElement"
-        class="w-full h-full overflow-y-auto overflow-hidden pb-20 no-scrollbar"
+        v-if="transfers.length === 0 && signer"
+        class="text-xl w-full h-full flex justify-center items-center text-sea-green/40"
       >
-        <div
-          v-if="transfers.length === 0 && signer"
-          class="text-xl w-full h-full flex justify-center items-center text-sea-green/40"
-        >
-          <span>Nothing here yet.</span>
-        </div>
+        <span>Nothing here yet.</span>
+      </div>
 
-        <div v-if="signer">
-          <template v-for="(group, groupIndex) of groupedAndSortedTransfers" :key="group.label">
-            <LazyWrapper
+      <div v-if="signer">
+        <template v-for="(group, groupIndex) of groupedAndSortedTransfers" :key="group.label">
+          <LazyWrapper
+            v-if="group.transfers.length > 0"
+            :root-element="listElement"
+            :threshold="0.0"
+          >
+            <div v-if="group.transfers.length > 0" class="text-xl text-center">
+              {{ group.label }}
+            </div>
+          </LazyWrapper>
+          <LazyWrapper
+            v-for="(transfer, groupTransferIndex) of group.transfers"
+            :key="transfer.requestInformation?.identifier ?? `${groupIndex}-${groupTransferIndex}`"
+            :threshold="0.0"
+            :root-element="listElement"
+          >
+            <TransferComponent
               v-if="group.transfers.length > 0"
-              :root-element="listElement"
-              :threshold="0.0"
-            >
-              <div v-if="group.transfers.length > 0" class="text-xl text-center">
-                {{ group.label }}
-              </div>
-            </LazyWrapper>
-            <LazyWrapper
-              v-for="(transfer, groupTransferIndex) of group.transfers"
-              :key="
-                transfer.requestInformation?.identifier ?? `${groupIndex}-${groupTransferIndex}`
-              "
-              :threshold="0.0"
-              :root-element="listElement"
-            >
-              <TransferComponent
-                v-if="group.transfers.length > 0"
-                :transfer="transfer"
-                class="my-3"
-                data-test="transfer"
-              />
-            </LazyWrapper>
-            <div v-if="group.transfers.length > 0" class="h-12"></div>
-          </template>
-        </div>
-        <div
-          v-else
-          class="text-xl w-full h-full flex justify-center items-center text-sea-green/40"
-        >
-          Connect wallet to view activity.
-        </div>
+              :transfer="transfer"
+              class="my-3"
+              data-test="transfer"
+            />
+          </LazyWrapper>
+          <div v-if="group.transfers.length > 0" class="h-12"></div>
+        </template>
+      </div>
+      <div v-else class="text-xl w-full h-full flex justify-center items-center text-sea-green/40">
+        Connect wallet to view activity.
       </div>
     </div>
   </div>
