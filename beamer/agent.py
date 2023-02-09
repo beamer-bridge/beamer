d58b2ed36bb9e68ca09c9a90c9e120c3d5e17bef
Tolga <bilbeyt@gmail.com>
2023-02-09 09:16:46 +0100

    agent: use chains to create event monitors

--- beamer/agent.py
+++ beamer/agent.py
@@ -1,8 +1,7 @@
 import threading
 from concurrent.futures import ThreadPoolExecutor
-
 import structlog
-from eth_typing import Address
+from eth_typing import Address, BlockNumber
 from web3 import Web3
 from web3.middleware import latest_block_based_cache_middleware
 
@@ -26,6 +25,12 @@ def _get_contracts_info(config: Config, web3: Web3) -> dict[str, ContractInfo]:
     return info
 
 
+def _get_deployment_block(contract_info: dict[str, ContractInfo]) -> BlockNumber:
+    request_manager_deployment_block = contract_info["RequestManager"].deployment_block
+    fill_manager_deployment_block = contract_info["FillManager"].deployment_block
+    return min(request_manager_deployment_block, fill_manager_deployment_block)
+
+
 class Agent:
     def __init__(self, config: Config):
         self._config = config
@@ -40,38 +45,45 @@ class Agent:
         self._task_pool = ThreadPoolExecutor(max_workers=1)
 
         w3_l1 = make_web3(self._config.l1_rpc_url, self._config.account)
-        w3_l2a = make_web3(self._config.l2a_rpc_url, self._config.account)
-        w3_l2b = make_web3(self._config.l2b_rpc_url, self._config.account)
+        w3_source = make_web3(self._config.l2a_rpc_url, self._config.account)
+        w3_target = make_web3(self._config.l2b_rpc_url, self._config.account)
+
+        source_chain_id = ChainId(w3_source.eth.chain_id)
+        target_chain_id = ChainId(w3_target.eth.chain_id)
+        chain_ids = {source_chain_id, target_chain_id}
 
         w3_l1.middleware_onion.add(latest_block_based_cache_middleware)
 
-        l2a_contracts_info = _get_contracts_info(self._config, w3_l2a)
-        l2b_contracts_info = _get_contracts_info(self._config, w3_l2b)
+        source_contracts_info = _get_contracts_info(self._config, w3_source)
+        target_contracts_info = _get_contracts_info(self._config, w3_target)
 
-        l2a_contracts = make_contracts(w3_l2a, l2a_contracts_info)
-        l2b_contracts = make_contracts(w3_l2b, l2b_contracts_info)
+        source_contracts = make_contracts(w3_source, source_contracts_info)
+        target_contracts = make_contracts(w3_target, target_contracts_info)
 
-        request_manager = l2a_contracts["RequestManager"]
-        fill_manager = l2b_contracts["FillManager"]
+        source_request_manager = source_contracts["RequestManager"]
+        source_fill_manager = source_contracts["FillManager"]
 
-        max_validity_period = request_manager.functions.MAX_VALIDITY_PERIOD().call()
+        target_fill_manager = target_contracts["FillManager"]
+        target_request_manager = target_contracts["RequestManager"]
+
+        max_validity_period = source_request_manager.functions.MAX_VALIDITY_PERIOD().call()
 
         if self._config.unsafe_fill_time >= max_validity_period:
             raise RuntimeError(f"Unsafe fill time must be less than {max_validity_period}")
-        if not fill_manager.functions.allowedLps(self._config.account.address).call():
+        if not target_fill_manager.functions.allowedLps(self._config.account.address).call():
             raise RuntimeError("Agent address is not whitelisted on FillManager")
-        if not request_manager.functions.allowedLps(self._config.account.address).call():
+        if not source_request_manager.functions.allowedLps(self._config.account.address).call():
             raise RuntimeError("Agent address is not whitelisted on RequestManager")
 
-        claim_request_extension = request_manager.functions.claimRequestExtension().call()
+        claim_request_extension = source_request_manager.functions.claimRequestExtension().call()
 
         self.context = Context(
             requests=Tracker(),
             claims=Tracker(),
-            source_chain_id=ChainId(w3_l2a.eth.chain_id),
-            target_chain_id=ChainId(w3_l2b.eth.chain_id),
-            request_manager=request_manager,
-            fill_manager=fill_manager,
+            source_chain_id=source_chain_id,
+            target_chain_id=target_chain_id,
+            request_manager=source_request_manager,
+            fill_manager=target_fill_manager,
             match_checker=self._config.token_match_checker,
             address=self._config.account.address,
             latest_blocks={},
@@ -85,37 +97,43 @@ class Agent:
 
         self._event_processor = EventProcessor(self.context)
 
-        self._event_monitor_l2a = EventMonitor(
-            web3=w3_l2a,
-            contracts=(request_manager,),
-            deployment_block=l2a_contracts_info["RequestManager"].deployment_block,
-            on_new_events=[self._event_processor.add_events],
-            on_sync_done=self._event_processor.mark_sync_done,
-            poll_period=POLL_PERIOD,
-        )
+        self._event_monitors: dict[ChainId, EventMonitor] = {}
 
-        self._event_monitor_l2b = EventMonitor(
-            web3=w3_l2b,
-            contracts=(fill_manager,),
-            deployment_block=l2b_contracts_info["FillManager"].deployment_block,
-            on_new_events=[self._event_processor.add_events],
-            on_sync_done=self._event_processor.mark_sync_done,
-            poll_period=POLL_PERIOD,
-        )
+        event_monitor_data = {
+            "on_new_events": [self._event_processor.add_events],
+            "on_sync_done": self._event_processor.mark_sync_done,
+            "poll_period": POLL_PERIOD,
+        }
+
+        for chain_id in chain_ids:
+            if chain_id == source_chain_id:
+                chain_related_data = {
+                    "web3": w3_source,
+                    "contracts": (source_request_manager, source_fill_manager),
+                    "deployment_block": _get_deployment_block(source_contracts_info),
+                }
+            else:
+                chain_related_data = {
+                    "web3": w3_target,
+                    "contracts": (target_request_manager, target_fill_manager),
+                    "deployment_block": _get_deployment_block(target_contracts_info),
+                }
+            event_monitor_data.update(chain_related_data)
+            self._event_monitors[chain_id] = EventMonitor(**event_monitor_data)  # type: ignore
 
     def start(self) -> None:
         assert self._stopped.is_set()
         beamer.metrics.init(self._config)
         self._event_processor.start()
-        self._event_monitor_l2a.start()
-        self._event_monitor_l2b.start()
+        for event_monitor in self._event_monitors.values():
+            event_monitor.start()
         self._stopped.clear()
 
     def stop(self) -> None:
         assert not self._stopped.is_set()
         self._event_processor.stop()
-        self._event_monitor_l2a.stop()
-        self._event_monitor_l2b.stop()
+        for event_monitor in self._event_monitors.values():
+            event_monitor.stop()
         self._task_pool.shutdown(wait=True, cancel_futures=False)
         self._init()
         self._stopped.set()
