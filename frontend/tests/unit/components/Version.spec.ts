bbab6a311bffa7661c5d49448a4b03d972d33d12
Gabriel Buragev <gabriel.buragev96@gmail.com>
2023-02-13 13:21:32 +0100

    frontend: use static repository url string
    Since `git ls-remote --get-url` command can sometimes return the SSH url
    and even contain GH access token inside of it, we had to move to a more
    stable approach by defining the GH repo url as a static string.
    Also, hiding the version in dev mode since it is irrelevant there.

--- frontend/tests/unit/components/Version.spec.ts
+++ frontend/tests/unit/components/Version.spec.ts
@@ -2,29 +2,57 @@ import { mount } from '@vue/test-utils';
 
 import Version from '@/components/Version.vue';
 
-const envCopy = Object.assign({}, APP_RELEASE);
+const globalCopy = Object.assign({}, APP_RELEASE);
+const importEnvCopy = Object.assign({}, import.meta.env);
 
-const resetEnvMock = () => {
-  Object.assign(APP_RELEASE, envCopy);
+const resetImportEnv = (): void => {
+  Object.assign(import.meta.env, importEnvCopy);
+};
+
+const resetGlobalVariables = (): void => {
+  Object.assign(APP_RELEASE, globalCopy);
 };
 
 describe('Version.vue', () => {
   afterEach(() => {
-    resetEnvMock();
+    resetImportEnv();
+    resetGlobalVariables();
   });
 
-  it('is hidden when no version is defined', () => {
+  it('is hidden when in development mode', () => {
+    Object.assign(import.meta.env, {
+      MODE: 'development',
+    });
+    const wrapper = mount(Version);
+
+    const commitHashEl = wrapper.find('[data-test="version"]');
+    expect(commitHashEl.exists()).toBe(false);
+  });
+
+  it('is visible when in production mode', () => {
+    Object.assign(import.meta.env, {
+      MODE: 'production',
+    });
+
+    const wrapper = mount(Version);
+
+    const commitHashEl = wrapper.find('[data-test="version"]');
+    expect(commitHashEl.exists()).toBe(true);
+  });
+
+  it('is hidden when commit hash is undefined', () => {
     Object.assign(APP_RELEASE, { COMMIT_HASH: undefined });
 
     const wrapper = mount(Version);
 
-    const commitHashEl = wrapper.find('[data-test="commit-hash"]');
+    const commitHashEl = wrapper.find('[data-test="version"]');
     expect(commitHashEl.exists()).toBe(false);
   });
-  it('displays the commit hash of the build', () => {
+
+  it('displays the release version of the build', () => {
     const wrapper = mount(Version);
 
-    const commitHashEl = wrapper.find('[data-test="commit-hash"]');
-    expect(commitHashEl.text()).toBe(APP_RELEASE.COMMIT_HASH);
+    const commitHashEl = wrapper.find('[data-test="version"]');
+    expect(commitHashEl.text()).toBe(APP_RELEASE.VERSION);
   });
 });
