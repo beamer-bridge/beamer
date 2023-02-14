056b159c9d4bef4b7313a6bb09fb909f6205d3c0
manuelwedler <manuel.wedler@googlemail.com>
2023-02-14 19:15:20 +0100

    frontend: Only show infinite token approval checkbox if allowance is not infinite yet

---
+++ frontend/src/composables/useTokenAllowance.ts
@@ -0,0 +1,36 @@
+import type { Ref } from 'vue';
+import { computed, ref, watch } from 'vue';
+
+import { getTokenAllowance } from '@/services/transactions/token';
+import type { IEthereumProvider } from '@/services/web3-provider';
+import type { Chain, Token } from '@/types/data';
+import type { TokenAmount } from '@/types/token-amount';
+import { UInt256 } from '@/types/uint-256';
+
+export function useTokenAllowance(
+  provider: Ref<IEthereumProvider | undefined>,
+  token: Ref<Token | undefined>,
+  sourceChain: Ref<Chain | undefined>,
+) {
+  const signerAddress = computed(() => provider.value?.signerAddress.value);
+  const allowance: Ref<TokenAmount | undefined> = ref(undefined);
+
+  const allowanceBelowMax = computed(() => allowance.value?.uint256.lt(UInt256.max()) ?? false);
+
+  async function updateTokenAllowance() {
+    if (token.value && provider.value && sourceChain.value && signerAddress.value) {
+      allowance.value = await getTokenAllowance(
+        provider.value,
+        token.value,
+        signerAddress.value,
+        sourceChain.value.requestManagerAddress,
+      );
+    } else {
+      allowance.value = undefined;
+    }
+  }
+
+  watch([token, sourceChain, signerAddress], updateTokenAllowance, { immediate: true });
+
+  return { allowance, allowanceBelowMax };
+}
