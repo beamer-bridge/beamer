934b7491e28ea17a5ce807eda1ad4364aff1a9ce
Tolga <bilbeyt@gmail.com>
2023-03-06 13:53:15 +0100

    contracts: Delete StandardToken.sol

--- contracts/contracts/StandardToken.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.12;
-
-import "OpenZeppelin/openzeppelin-contracts@4.8.0/contracts/token/ERC20/ERC20.sol";
-
-// solhint-disable-next-line no-empty-blocks
-abstract contract StandardToken is ERC20 {
-
-}
