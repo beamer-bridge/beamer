5c4a3322a82f4f28dceacf865b0789921ce7539d
Ivan Stanković <ivan@brainbot.com>
2023-03-09 11:46:26 +0100

    beamer: tests: add a test for HTTP 413 handling

---
+++ beamer/tests/agent/test_rpc_errors.py
@@ -0,0 +1,51 @@
+import functools
+import itertools
+import json
+
+import ape
+import structlog.testing
+
+from requests.exceptions import HTTPError
+
+from beamer.agent.agent import Agent
+from beamer.tests.util import HTTPProxy
+
+
+def _post_with_error_code(code, counter, handler, url, post_body):
+    request_data = json.loads(post_body)
+    if request_data["method"] == "eth_getLogs":
+        # We have to send an error at least 5 times in order for the error to end
+        # up in our log. This is because the default web3 HTTPProvider middleware
+        # will retry up to 5 times before returning the error to us.
+        count = next(counter)
+        if count < 5:
+            handler.send_error(code)
+            handler.wfile.flush()
+            return
+
+    response = handler.forward_request(url, post_body)
+    if response is not None:
+        handler.complete(response)
+
+
+def test_413_error(config):
+    ape.chain.mine(200)
+
+    post_with_error = functools.partial(_post_with_error_code, 413, itertools.count())
+
+    proxy_l2a = HTTPProxy(config.rpc_urls["l2a"], post_with_error)
+    proxy_l2a.start()
+
+    config.rpc_urls["l2a"] = proxy_l2a.url()
+
+    agent = Agent(config)
+
+    with structlog.testing.capture_logs() as captured_logs:
+        agent.start()
+        agent.stop()
+
+    proxy_l2a.stop()
+    assert any(
+        isinstance(log.get("exc"), HTTPError) and log["exc"].response.status_code == 413
+        for log in captured_logs
+    )
