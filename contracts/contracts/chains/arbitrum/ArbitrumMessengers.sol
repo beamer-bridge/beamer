fd21e791da2a2fe86910354db05eb28113cc0bf1
Ivan Stanković <ivan@brainbot.com>
2023-03-23 17:49:50 +0100

    contracts: require solidity at least 0.8.19

--- contracts/contracts/chains/arbitrum/ArbitrumMessengers.sol
+++ contracts/contracts/chains/arbitrum/ArbitrumMessengers.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.12;
+pragma solidity ^0.8.19;
 
 import "OpenZeppelin/openzeppelin-contracts@4.8.0/contracts/vendor/arbitrum/IArbSys.sol";
 import "OpenZeppelin/openzeppelin-contracts@4.8.0/contracts/vendor/arbitrum/IBridge.sol";
