9a08836ea96817b5c960f7a8a9b1b0d02b04df6e
Gabriel Buragev <gabriel.buragev96@gmail.com>
2023-04-13 10:11:44 +0200

    frontend: change banner text

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,7 @@
     <div
       class="flex h-16 w-full flex-col justify-center bg-fire text-center text-xl font-semibold text-rosa lg:text-2xl"
     >
-      ✦ Beamer is now live on Arbitrum! ✦
+      ✦ Layer up! You can now bridge from Ethereum L1 to rollups ✦
     </div>
     <div class="container mx-auto max-w-5xl">
       <a href="https://beamerbridge.com" target="_blank">
