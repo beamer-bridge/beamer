e74efd6428bf0884e289dd934115d608c8c68f95
Ivan Stanković <ivan@brainbot.com>
2023-05-09 14:09:23 +0200

    beamer: deploy: add a deployment config module

---
+++ beamer/deploy/config.py
@@ -0,0 +1,81 @@
+import json
+from dataclasses import dataclass, field
+from pathlib import Path
+
+import apischema
+from apischema import ValidationError, schema
+from apischema.metadata import validators
+from eth_typing import ChecksumAddress
+from eth_utils import is_checksum_address
+
+from beamer.typing import URL, ChainId
+
+
+def _validate_token_address(address: str) -> None:
+    if address != "mintable_token" and not is_checksum_address(address):
+        raise ValidationError(f"expected either 'mintable_token' or a checksum address: {address}")
+
+
+@dataclass
+class _BaseChain:
+    name: str
+    rpc: str
+    chain_id: ChainId = field(metadata=schema(min=1))
+
+
+@dataclass
+class _RequestManagerArgs:
+    claim_stake: float = field(metadata=schema(min=0))
+    claim_request_extension: int = field(metadata=schema(min=0))
+    claim_period: int = field(metadata=schema(min=0))
+    challenge_period_extension: int = field(metadata=schema(min=0))
+
+
+@dataclass
+class _Fees:
+    min_fee_ppm: int = field(metadata=schema(min=0))
+    lp_fee_ppm: int = field(metadata=schema(min=0))
+    protocol_fee_ppm: int = field(metadata=schema(min=0))
+
+
+@dataclass
+class _Token:
+    token_address: str | ChecksumAddress = field(metadata=validators(_validate_token_address))
+    transfer_limit: int = field(metadata=schema(min=0))
+    eth_in_token: float = field(metadata=schema(min=0))
+
+
+@dataclass
+class Chain:
+    name: str
+    chain_id: ChainId = field(metadata=schema(min=1))
+    l1_messenger: str | tuple[str, ...]
+    l2_messenger: str | tuple[str, ...]
+    finality_period: int = field(metadata=schema(min=1))
+    transfer_cost: int = field(metadata=schema(min=0))
+    target_weight_ppm: int = field(metadata=schema(min=0))
+    request_manager_arguments: _RequestManagerArgs
+    fees: _Fees
+    tokens: tuple[_Token, ...] = field(metadata=schema(min_items=1))
+
+    @staticmethod
+    def from_file(config_file: Path) -> "Chain":
+        with open(config_file) as f:
+            data = json.load(f)
+
+        try:
+            chain = apischema.deserialize(Chain, data)
+        except ValidationError as exc:
+            raise ConfigValidationError from exc
+        return chain
+
+
+class ConfigValidationError(Exception):
+    def __str__(self) -> str:
+        assert isinstance(self.__cause__, ValidationError)
+        return "\n".join(map(str, self.__cause__.errors))  # pylint: disable=no-member
+
+
+def load_rpc_info(path: Path) -> dict[ChainId, URL]:
+    info = json.loads(path.read_text())
+    return {ChainId(int(k)): URL(v) for k, v in info.items()}
