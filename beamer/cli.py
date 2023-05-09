17937c9d510b0aa4c0b5fece9c5165ccc5eca89e
Ivan Stanković <ivan@brainbot.com>
2023-05-09 14:07:45 +0200

    beamer: hook up the new 'deploy-base' and 'deploy' commands

--- beamer/cli.py
+++ beamer/cli.py
@@ -1,6 +1,7 @@
 import click
 
 from beamer.agent import commands as agent_commands
+from beamer.deploy import commands as deploy_commands
 from beamer.health import commands as health_commands
 
 
@@ -11,3 +12,5 @@ def main() -> None:
 
 main.add_command(agent_commands.agent)
 main.add_command(health_commands.monitor)
+main.add_command(deploy_commands.deploy)
+main.add_command(deploy_commands.deploy_base)
