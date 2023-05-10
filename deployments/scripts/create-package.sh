d78330a61f018e7e0c65ea277b1f1ceb4a9dbc56
Daniel Dimitrov <daniel@compojoom.com>
2023-05-10 11:46:49 +0200

    deployments: create npm package
    The package only contains the artifacts and abis and doesn’t expose any JS API.
    Users can include the files as they wish in their projects.

---
+++ deployments/scripts/create-package.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+rm -rf dist
+mkdir -p dist/abis/{goerli,mainnet}
+mkdir -p dist/artifacts
+
+cp -r artifacts/ dist/artifacts
+
+python ../scripts/generate-abi.py --only-abi artifacts/mainnet dist/abis/mainnet
+cp goerli/*.* dist/abis/goerli
