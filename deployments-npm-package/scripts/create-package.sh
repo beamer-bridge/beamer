22227852a5201918caa0ad672d41f47693ab8ce1
Daniel Dimitrov <daniel@compojoom.com>
2023-05-19 13:37:59 +0200

    deployments: add the git-commit-version to package

--- deployments-npm-package/scripts/create-package.sh
+++ deployments-npm-package/scripts/create-package.sh
@@ -8,3 +8,5 @@ cp -r ../deployments/artifacts/. dist/artifacts
 
 python ../scripts/generate-abi.py --only-abi ../deployments/artifacts/mainnet dist/abis/mainnet
 cp ../deployments/goerli/*.* dist/abis/goerli
+
+git rev-parse HEAD > git-commit-version.txt
