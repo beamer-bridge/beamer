eb2b693ea447c2d3ec08c73308b5274810d27204
Gabriel Buragev <gabriel.buragev96@gmail.com>
2023-06-06 00:34:10 +0200

    relayer: add missing tests

--- relayer/tests/unit/cli/programs/relay.spec.ts
+++ relayer/tests/unit/cli/programs/relay.spec.ts
@@ -19,38 +19,41 @@ const validOptions: ProgramOptions = {
   l2TransactionHash: getRandomTransactionHash(),
 };
 
-describe("validateArgs", () => {
-  it("gives no error for valid ProgramOptions", () => {
-    const errors = RelayerProgram.validateArgs(validOptions);
-    expect(errors).toEqual([]);
-  });
-
-  it("gives an error for invalid transactions hashes", () => {
-    const invalidTransactionHashes = [
-      getRandomTransactionHash() + "A",
-      getRandomTransactionHash().slice(2),
-    ];
-
-    for (const hash of invalidTransactionHashes) {
-      const errors = RelayerProgram.validateArgs(
-        Object.assign({}, validOptions, { l2TransactionHash: hash }),
-      );
-
-      expect(errors).toEqual([
-        `Invalid argument value for "--l2-transaction-hash": "${hash}" doesn't look like a txn hash...`,
-      ]);
-    }
-  });
-});
+const L1_CHAIN_ID = 1;
 
 describe("RelayerProgram", () => {
+  describe("validateArgs", () => {
+    it("gives no error for valid ProgramOptions", () => {
+      const errors = RelayerProgram.validateArgs(validOptions);
+      expect(errors).toEqual([]);
+    });
+
+    it("gives an error for invalid transactions hashes", () => {
+      const invalidTransactionHashes = [
+        getRandomTransactionHash() + "A",
+        getRandomTransactionHash().slice(2),
+      ];
+
+      for (const hash of invalidTransactionHashes) {
+        const errors = RelayerProgram.validateArgs(
+          Object.assign({}, validOptions, { l2TransactionHash: hash }),
+        );
+
+        expect(errors).toEqual([
+          `Invalid argument value for "--l2-transaction-hash": "${hash}" doesn't look like a txn hash...`,
+        ]);
+      }
+    });
+  });
+
   it("can be created from args", async () => {
     const fromChainId = Number(Object.keys(SERVICES)[0]);
     const toChainId = Number(Object.keys(SERVICES)[3]);
 
     (getNetworkId as jest.Mock)
       .mockResolvedValueOnce(fromChainId)
-      .mockResolvedValueOnce(toChainId);
+      .mockResolvedValueOnce(toChainId)
+      .mockResolvedValueOnce(L1_CHAIN_ID);
 
     const program = await RelayerProgram.createFromArgs(validOptions);
 
@@ -59,13 +62,39 @@ describe("RelayerProgram", () => {
     expect(program.l2TransactionHash).toBe(validOptions.l2TransactionHash);
   });
 
+  describe("createFromArgs()", () => {
+    it("creates a relayer service for each direction and initializes them with the right values", async () => {
+      const fromChainId = Number(Object.keys(SERVICES)[0]);
+      const toChainId = Number(Object.keys(SERVICES)[3]);
+
+      (getNetworkId as jest.Mock)
+        .mockResolvedValueOnce(fromChainId)
+        .mockResolvedValueOnce(toChainId)
+        .mockResolvedValueOnce(L1_CHAIN_ID);
+
+      const program = await RelayerProgram.createFromArgs(validOptions);
+
+      const relayerFrom = program.l2RelayerFrom;
+      const relayerTo = program.l2RelayerTo;
+
+      expect(relayerFrom.l1ChainId).toBe(L1_CHAIN_ID);
+      expect(relayerFrom.l2ChainId).toBe(fromChainId);
+      expect(relayerFrom.destinationChainId).toBe(toChainId);
+
+      expect(relayerTo.l1ChainId).toBe(L1_CHAIN_ID);
+      expect(relayerTo.l2ChainId).toBe(toChainId);
+      expect(relayerTo.destinationChainId).toBe(fromChainId);
+    });
+  });
+
   describe("run()", () => {
     it("runs all the necessary methods for successful relaying of a transaction", async () => {
       const fromChainId = Number(Object.keys(SERVICES)[0]);
       const toChainId = Number(Object.keys(SERVICES)[3]);
       (getNetworkId as jest.Mock)
         .mockResolvedValueOnce(fromChainId)
-        .mockResolvedValueOnce(toChainId);
+        .mockResolvedValueOnce(toChainId)
+        .mockResolvedValueOnce(L1_CHAIN_ID);
 
       const program = await RelayerProgram.createFromArgs(validOptions);
 
@@ -87,7 +116,8 @@ describe("RelayerProgram", () => {
       const toChainId = Number(Object.keys(SERVICES)[3]);
       (getNetworkId as jest.Mock)
         .mockResolvedValueOnce(fromChainId)
-        .mockResolvedValueOnce(toChainId);
+        .mockResolvedValueOnce(toChainId)
+        .mockResolvedValueOnce(L1_CHAIN_ID);
 
       const program = await RelayerProgram.createFromArgs(validOptions);
 
