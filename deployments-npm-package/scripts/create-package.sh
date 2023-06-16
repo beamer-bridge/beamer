8badbb32621f25a68619e03f57c926e2ce8b664e
Gabriel Buragev <gabriel.buragev96@gmail.com>
2023-06-16 17:01:09 +0200

    npm: use generate script for goerli artifacts

--- deployments-npm-package/scripts/create-package.sh
+++ deployments-npm-package/scripts/create-package.sh
@@ -7,6 +7,6 @@ mkdir -p dist/artifacts
 cp -r ../deployments/artifacts/. dist/artifacts
 
 python ../scripts/generate-abi.py --only-abi ../deployments/artifacts/mainnet dist/abis/mainnet
-cp ../deployments/goerli/*.* dist/abis/goerli
+python ../scripts/generate-abi.py --only-abi ../deployments/artifacts/goerli dist/abis/goerli
 
 git rev-parse HEAD > git-commit-version.txt
