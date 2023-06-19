fd3f663d24c5b6f9b86982ec8cb404237672c412
manuelwedler <manuel.wedler@googlemail.com>
2023-06-19 17:11:56 +0200

    relayer: Return l1 transaction hash in isCompleted check of Arbitrum relayer service

--- relayer/src/services/relayer/ethereum.ts
+++ relayer/src/services/relayer/ethereum.ts
@@ -3,38 +3,20 @@ import { keccak256 } from "ethers/lib/utils";
 
 import type { EthereumL2Messenger } from "../../../types-gen/contracts";
 import { EthereumL2Messenger__factory, Resolver__factory } from "../../../types-gen/contracts";
-import type { TypedEvent, TypedEventFilter } from "../../../types-gen/contracts/common";
-import { parseFillInvalidatedEvent } from "../../common/events/FillInvalidated";
-import { parseRequestFilledEvent } from "../../common/events/RequestFilled";
 import type { TransactionHash } from "../types";
 import { BaseRelayerService, RelayStep } from "../types";
 
-type RelayCallParams = {
-  requestId: string;
-  fillId: string;
-  sourceChainId: BigNumber;
-  filler: string;
-};
-
-const L1_CONTRACTS: Record<
-  number,
-  { ETHEREUM_L2_MESSENGER: string; RESOLVER_DEPLOY_BLOCK_NUMBER: number }
-> = {
+const L1_CONTRACTS: Record<number, { ETHEREUM_L2_MESSENGER: string }> = {
   1: {
     ETHEREUM_L2_MESSENGER: "0x3222C9a1e5d7856FCBc551A30a63634e7Fd634Da",
-    RESOLVER_DEPLOY_BLOCK_NUMBER: 16946576,
   },
   5: {
     ETHEREUM_L2_MESSENGER: "0x6064A4d69D6535981F1091Fa2243d9a106046e46",
-    RESOLVER_DEPLOY_BLOCK_NUMBER: 9066315,
   },
   1337: {
     ETHEREUM_L2_MESSENGER: process.env.ETHEREUM_L2_MESSENGER || "",
-    RESOLVER_DEPLOY_BLOCK_NUMBER: 0,
   },
 };
-
-const FILTER_BLOCKS_PER_ITERATION = 5000;
 
 export class EthereumRelayerService extends BaseRelayerService {
   prepareStep = undefined;
@@ -51,81 +33,6 @@ export class EthereumRelayerService extends BaseRelayerService {
 
     const ethereumMessengerAddress = L1_CONTRACTS[this.l2ChainId].ETHEREUM_L2_MESSENGER;
     this.messenger = EthereumL2Messenger__factory.connect(ethereumMessengerAddress, this.l2Wallet);
-  }
-
-  async parseEventDataFromTxHash(
-    l1TransactionHash: TransactionHash,
-  ): Promise<RelayCallParams | null> {
-    const receipt = await this.l2RpcProvider.getTransactionReceipt(l1TransactionHash);
-
-    if (!receipt) {
-      throw new Error(`Transaction "${l1TransactionHash}" cannot be found on Ethereum...`);
-    }
-
-    const requestFilledEvent = parseRequestFilledEvent(receipt.logs);
-
-    if (requestFilledEvent) {
-      return {
-        requestId: requestFilledEvent.requestId,
-        fillId: requestFilledEvent.fillId,
-        sourceChainId: requestFilledEvent.sourceChainId,
-        filler: requestFilledEvent.filler,
-      };
-    }
-
-    const fillInvalidatedEvent = parseFillInvalidatedEvent(receipt.logs);
-
-    if (fillInvalidatedEvent && this.destinationChainId) {
-      return {
-        ...fillInvalidatedEvent,
-        sourceChainId: BigNumber.from(this.destinationChainId),
-        filler: "0x0000000000000000000000000000000000000000",
-      };
-    }
-
-    return null;
-  }
-
-  private async findTransactionHashForMessage(
-    requestId: string,
-    fillId: string,
-    sourceChainId: BigNumber,
-    filler: string,
-    fillChainId: BigNumber,
-    resolverAddress: string,
-  ): Promise<string | undefined> {
-    const resolver = Resolver__factory.connect(resolverAddress, this.l1Wallet);
-    const currentBlock = await this.l1Wallet.provider.getBlock("latest");
-    const resolverDeployBlockNumber = L1_CONTRACTS[this.l1ChainId].RESOLVER_DEPLOY_BLOCK_NUMBER;
-    let currentBlockNumber = currentBlock.number;
-
-    while (currentBlockNumber > resolverDeployBlockNumber) {
-      const events = await resolver.queryFilter(
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        "Resolution" as TypedEventFilter<TypedEvent<any, any>>,
-        currentBlockNumber - FILTER_BLOCKS_PER_ITERATION,
-        currentBlockNumber,
-      );
-
-      for (const event of events) {
-        if (event.event === "Resolution") {
-          const args = event.args;
-
-          if (
-            sourceChainId.eq(args.sourceChainId) &&
-            fillChainId.eq(args.fillChainId) &&
-            args.requestId === requestId &&
-            args.filler === filler &&
-            args.fillId === fillId
-          ) {
-            return event.transactionHash;
-          }
-        }
-      }
-      currentBlockNumber -= FILTER_BLOCKS_PER_ITERATION;
-    }
-
-    return undefined;
   }
 
   private createMessageHash(
@@ -147,20 +54,10 @@ export class EthereumRelayerService extends BaseRelayerService {
     return keccak256(encodedCall);
   }
 
-  private async getCallParameters(l1TransactionHash: TransactionHash): Promise<RelayCallParams> {
-    const callParameters = await this.parseEventDataFromTxHash(l1TransactionHash);
-    if (!callParameters) {
-      throw new Error(
-        "Couldn't find a matching event (RequestFilled | FillInvalidated) in the transaction logs.",
-      );
-    }
-    return callParameters;
-  }
-
   private async isRelayCompleted(
     l1TransactionHash: TransactionHash,
   ): Promise<TransactionHash | false> {
-    const callParameters = await this.getCallParameters(l1TransactionHash);
+    const callParameters = await this.parseFillEventDataFromTxHash(l1TransactionHash);
 
     const l2ChainId = await this.getL2ChainId();
     const parameters = [
@@ -177,11 +74,9 @@ export class EthereumRelayerService extends BaseRelayerService {
     if (isMessageRelayed) {
       console.log("Message has already been relayed..");
 
-      const resolverAddress = await this.messenger.resolver();
-      const transactionHash = await this.findTransactionHashForMessage(
+      const transactionHash = await this.findL1TransactionHashForMessage(
         ...parameters,
         BigNumber.from(l2ChainId),
-        resolverAddress,
       );
       if (!transactionHash) {
         throw new Error(
@@ -197,7 +92,7 @@ export class EthereumRelayerService extends BaseRelayerService {
   }
 
   private async relayTxToL1(l1TransactionHash: TransactionHash): Promise<TransactionHash> {
-    const callParameters = await this.getCallParameters(l1TransactionHash);
+    const callParameters = await this.parseFillEventDataFromTxHash(l1TransactionHash);
 
     console.log("Ethereum execution");
 
--- relayer/src/services/types.ts
+++ relayer/src/services/types.ts
@@ -1,7 +1,11 @@
 import type { Provider } from "@ethersproject/providers";
 import { JsonRpcProvider } from "@ethersproject/providers";
-import { Wallet } from "ethers";
+import { BigNumber, Wallet } from "ethers";
 
+import { Resolver__factory } from "../../types-gen/contracts";
+import type { TypedEvent, TypedEventFilter } from "../../types-gen/contracts/common";
+import { parseFillInvalidatedEvent } from "../common/events/FillInvalidated";
+import { parseRequestFilledEvent } from "../common/events/RequestFilled";
 import { ExtendedJsonRpcProvider } from "../ethers/json-rpc-provider";
 import type { ArbitrumRelayerService } from "./relayer/arbitrum";
 import type { BobaRelayerService } from "./relayer/boba";
@@ -17,6 +21,31 @@ export type ExtendedRelayerService =
   | typeof OptimismRelayerService
   | typeof EthereumRelayerService
   | typeof PolygonZKEvmRelayerService;
+
+export type RelayCallParams = {
+  requestId: string;
+  fillId: string;
+  sourceChainId: BigNumber;
+  filler: string;
+};
+
+const L1_CONTRACTS: Record<number, { RESOLVER: string; RESOLVER_DEPLOY_BLOCK_NUMBER: number }> = {
+  1: {
+    RESOLVER: "0xCb60516819a28431233195A8b7E0227C288B61AD",
+    // TODO: import from `deployments` npm package once ready
+    RESOLVER_DEPLOY_BLOCK_NUMBER: 16946576,
+  },
+  5: {
+    RESOLVER: "0xDCBF185279C7D0F2d448cb674d9429323c8120DC",
+    RESOLVER_DEPLOY_BLOCK_NUMBER: 9066315,
+  },
+  1337: {
+    RESOLVER: process.env.RESOLVER || "",
+    RESOLVER_DEPLOY_BLOCK_NUMBER: 0,
+  },
+};
+
+const FILTER_BLOCKS_PER_ITERATION = 5000;
 
 export abstract class BaseRelayerService {
   readonly l1RpcUrl: string;
@@ -71,6 +100,83 @@ export abstract class BaseRelayerService {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   addCustomNetwork(_filePath: string): void {
     return;
+  }
+
+  protected async parseFillEventDataFromTxHash(
+    l2TransactionHash: TransactionHash,
+  ): Promise<RelayCallParams> {
+    const receipt = await this.l2RpcProvider.getTransactionReceipt(l2TransactionHash);
+
+    if (!receipt) {
+      throw new Error(`Transaction "${l2TransactionHash}" cannot be found on Ethereum...`);
+    }
+
+    const requestFilledEvent = parseRequestFilledEvent(receipt.logs);
+
+    if (requestFilledEvent) {
+      return {
+        requestId: requestFilledEvent.requestId,
+        fillId: requestFilledEvent.fillId,
+        sourceChainId: requestFilledEvent.sourceChainId,
+        filler: requestFilledEvent.filler,
+      };
+    }
+
+    const fillInvalidatedEvent = parseFillInvalidatedEvent(receipt.logs);
+
+    if (fillInvalidatedEvent && this.destinationChainId) {
+      return {
+        ...fillInvalidatedEvent,
+        sourceChainId: BigNumber.from(this.destinationChainId),
+        filler: "0x0000000000000000000000000000000000000000",
+      };
+    }
+
+    throw new Error(
+      "Couldn't find a matching event (RequestFilled | FillInvalidated) in the transaction logs.",
+    );
+  }
+
+  protected async findL1TransactionHashForMessage(
+    requestId: string,
+    fillId: string,
+    sourceChainId: BigNumber,
+    filler: string,
+    fillChainId: BigNumber,
+  ): Promise<string | undefined> {
+    const resolverAddress = L1_CONTRACTS[this.l1ChainId].RESOLVER;
+    const resolver = Resolver__factory.connect(resolverAddress, this.l1Wallet);
+    const currentBlock = await this.l1Wallet.provider.getBlock("latest");
+    const resolverDeployBlockNumber = L1_CONTRACTS[this.l1ChainId].RESOLVER_DEPLOY_BLOCK_NUMBER;
+    let currentBlockNumber = currentBlock.number;
+
+    while (currentBlockNumber > resolverDeployBlockNumber) {
+      const events = await resolver.queryFilter(
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        "Resolution" as TypedEventFilter<TypedEvent<any, any>>,
+        currentBlockNumber - FILTER_BLOCKS_PER_ITERATION,
+        currentBlockNumber,
+      );
+
+      for (const event of events) {
+        if (event.event === "Resolution") {
+          const args = event.args;
+
+          if (
+            sourceChainId.eq(args.sourceChainId) &&
+            fillChainId.eq(args.fillChainId) &&
+            args.requestId === requestId &&
+            args.filler === filler &&
+            args.fillId === fillId
+          ) {
+            return event.transactionHash;
+          }
+        }
+      }
+      currentBlockNumber -= FILTER_BLOCKS_PER_ITERATION;
+    }
+
+    return undefined;
   }
 }
 
