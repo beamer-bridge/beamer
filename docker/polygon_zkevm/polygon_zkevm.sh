8a204608942198576d031ed076668459b4e88e77
Daniel Dimitrov <daniel@compojoom.com>
2023-06-23 12:59:20 +0200

    docker: use variables for replace in sed

--- docker/polygon_zkevm/polygon_zkevm.sh
+++ docker/polygon_zkevm/polygon_zkevm.sh
@@ -49,8 +49,8 @@ fund_account() {
   echo "Fund account"
   DEPOSIT_FILE="${POLYGON_ZKEVM}/test/scripts/deposit/main.go"
   # Replace the hardcoded address and private key with our L1 account
-  sed -i'' -e 's/0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266/0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC/' "${DEPOSIT_FILE}"
-  sed -i'' -e 's/0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80/0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a/' "${DEPOSIT_FILE}"
+  sed -i'' -e "s/0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266/$ADDRESS/" "${DEPOSIT_FILE}"
+  sed -i'' -e "s/0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80/$PRIVKEY/" "${DEPOSIT_FILE}"
 
   cd ${POLYGON_ZKEVM}
   go run test/scripts/deposit/main.go
