70d4316777e9e3fe3d7bc72072985f476e478497
Ivan Stanković <ivan@brainbot.com>
2023-07-07 16:52:46 +0200

    beamer: tests: make tests work again after the config changes

--- beamer/tests/agent/multiple_processors/_test_slave.py
+++ beamer/tests/agent/multiple_processors/_test_slave.py
@@ -2,14 +2,18 @@ import json
 import os
 import sys
 import time
+from pathlib import Path
+
 import ape
 
+from beamer.tests.agent.utils import generate_artifacts
 from beamer.tests.util import alloc_accounts, make_request
 
 
 def test_sending_request_slave(contracts, token, chain_params):
     requester, target = alloc_accounts(2)
     chain_id = ape.chain.chain_id
+    artifacts_dir = Path(os.environ["ARTIFACTS_DIR"])
     request_count = int(os.environ["REQUEST_COUNT"])
     first_chain_id = int(os.environ["FIRST_CHAIN_ID"])
     for i in range(first_chain_id, first_chain_id + request_count):
@@ -17,6 +21,8 @@ def test_sending_request_slave(contracts, token, chain_params):
             continue
         contracts.request_manager.updateChain(i, *chain_params)
         make_request(contracts.request_manager, token, requester, target, 1, target_chain_id=i)
+
+    generate_artifacts(artifacts_dir, contracts)
     print("Chain is ready", flush=True)
 
     while True:
