56f110bc8642a881c1187befdb263d793406b49e
manuelwedler <manuel.wedler@googlemail.com>
2023-07-24 17:54:00 +0200

    frontend: `SubsidizedTransfer`: Only overwrite transaction wrapper calls
    In the former implementation things could easily go wrong because the functions of the `Transfer` class are quite big. It was not easily seeable what happens when the `requestManagerAddress` was overwritten by the `feeSubAddress`. The new implementation should be fail-safer. Only the necessary transaction wrappers are overwritten by providing the `feeSubAddress` in the call to the transaction function.

--- frontend/src/actions/transfers/subsidized-transfer.ts
+++ frontend/src/actions/transfers/subsidized-transfer.ts
@@ -1,36 +1,72 @@
+import { sendRequestTransaction, withdrawRequest } from '@/services/transactions/request-manager';
+import { ensureTokenAllowance, isAllowanceApproved } from '@/services/transactions/token';
 import type { IEthereumProvider } from '@/services/web3-provider';
+import type { EthereumAddress } from '@/types/data';
 import { UInt256 } from '@/types/uint-256';
 
 import { type TransferData, Transfer } from './transfer';
 
 export class SubsidizedTransfer extends Transfer {
   readonly feeSubAddress: string;
-  readonly originalRequestManagerAddress: string;
 
   constructor(data: SubsidizedTransferData) {
     if (!data.sourceChain.feeSubAddress) {
       throw new Error('Please provide a fee subsidy contract address.');
     }
     super(data);
-    this.originalRequestManagerAddress = data.sourceChain.requestManagerAddress;
     this.feeSubAddress = data.sourceChain.feeSubAddress;
     Object.assign(this.fees.uint256, this.fees.uint256.multiply(new UInt256(0)));
   }
 
-  async ensureTokenAllowance(provider: IEthereumProvider): Promise<void> {
-    if (this.feeSubAddress) {
-      this.sourceChain.requestManagerAddress = this.feeSubAddress;
-    }
-    await super.ensureTokenAllowance(provider);
-    this.sourceChain.requestManagerAddress = this.originalRequestManagerAddress;
+  protected async callWithdrawRequest(
+    provider: IEthereumProvider,
+    requestIdentifier: string,
+  ): Promise<void> {
+    await withdrawRequest(provider, this.feeSubAddress, requestIdentifier);
   }
 
-  async sendRequestTransaction(provider: IEthereumProvider): Promise<void> {
-    if (this.feeSubAddress) {
-      this.sourceChain.requestManagerAddress = this.feeSubAddress;
-    }
-    await super.sendRequestTransaction(provider);
-    this.sourceChain.requestManagerAddress = this.originalRequestManagerAddress;
+  protected async callEnsureTokenAllowance(
+    provider: IEthereumProvider,
+    tokenAddress: string,
+    amount: UInt256,
+  ): Promise<string | undefined> {
+    return await ensureTokenAllowance(provider, tokenAddress, this.feeSubAddress, amount);
+  }
+
+  protected async callIsAllowanceApproved(
+    provider: IEthereumProvider,
+    tokenAddress: string,
+    ownerAddress: string,
+    amount: UInt256,
+  ): Promise<boolean> {
+    return await isAllowanceApproved(
+      provider,
+      tokenAddress,
+      ownerAddress,
+      this.feeSubAddress,
+      amount,
+    );
+  }
+
+  protected async callSendRequestTransaction(
+    provider: IEthereumProvider,
+    amount: UInt256,
+    targetChainIdentifier: number,
+    sourceTokenAddress: EthereumAddress,
+    targetTokenAddress: EthereumAddress,
+    targetAccount: EthereumAddress,
+    validityPeriod: UInt256,
+  ): Promise<string> {
+    return await sendRequestTransaction(
+      provider,
+      amount,
+      targetChainIdentifier,
+      this.feeSubAddress,
+      sourceTokenAddress,
+      targetTokenAddress,
+      targetAccount,
+      validityPeriod,
+    );
   }
 
   public encode(): SubsidizedTransferData {
@@ -38,14 +74,12 @@ export class SubsidizedTransfer extends Transfer {
     return {
       ...encodedTransferData,
       feeSubAddress: this.feeSubAddress,
-      originalRequestManagerAddress: this.originalRequestManagerAddress,
     };
   }
 }
 
 export type SubsidizedTransferData = TransferData & {
   feeSubAddress: string;
-  originalRequestManagerAddress: string;
 };
 
 export function isSubsidizedTransferData(
