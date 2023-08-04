864905f7b52b9e66e0aa50539adf7947a6852435
Daniel Dimitrov <daniel@compojoom.com>
2023-08-04 11:13:01 +0200

    deployments: create package with abi bytecode
    Breaking change!
    
    Currently we generate the abis without the bytecode. We do this because the frontend doesn’t use the bytecode. However the agent and backend code needs the bytecode.
    
    The abis now generated in /abis/ will have the bytecode by default.
    If someone needs abis without the bytecode they are in the abis-without-bytecode folder.

--- deployments-npm-package/scripts/create-package.sh
+++ deployments-npm-package/scripts/create-package.sh
@@ -2,11 +2,15 @@
 
 rm -rf dist
 mkdir -p dist/abis/{goerli,mainnet}
+mkdir -p dist/abis-without-bytecode/{goerli,mainnet}
 mkdir -p dist/artifacts
 
 cp -r ../deployments/artifacts/. dist/artifacts
 
-python ../scripts/generate-abi.py --only-abi ../deployments/artifacts/mainnet dist/abis/mainnet
-python ../scripts/generate-abi.py --only-abi ../deployments/artifacts/goerli dist/abis/goerli
+python ../scripts/generate-abi.py ../deployments/artifacts/mainnet dist/abis/mainnet
+python ../scripts/generate-abi.py ../deployments/artifacts/goerli dist/abis/goerli
+
+python ../scripts/generate-abi.py --only-abi ../deployments/artifacts/mainnet dist/abis-without-bytecode/mainnet
+python ../scripts/generate-abi.py --only-abi ../deployments/artifacts/goerli dist/abis-without-bytecode/goerli
 
 git rev-parse HEAD > git-commit-version.txt
