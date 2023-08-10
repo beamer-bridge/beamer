e4a208aaa0ecf1fce7ccddc89fb47f9bda3fb901
Ivan Stanković <ivan@brainbot.com>
2023-08-10 17:45:03 +0200

    beamer: cli: hook up the new check command group

--- beamer/cli.py
+++ beamer/cli.py
@@ -1,6 +1,7 @@
 import click
 
 from beamer.agent import commands as agent_commands
+from beamer.check import commands as check_commands
 from beamer.config import commands as config_commands
 from beamer.deploy import commands as deploy_commands
 from beamer.health import commands as health_commands
@@ -16,3 +17,4 @@ main.add_command(health_commands.monitor)
 main.add_command(deploy_commands.deploy)
 main.add_command(deploy_commands.deploy_base)
 main.add_command(config_commands.config)
+main.add_command(check_commands.check)
