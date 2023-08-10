77a142ff1630dac7b0dec9914156d16e98d5599a
Daniel Dimitrov <daniel@compojoom.com>
2023-08-10 17:03:13 +0200

    subgraph: withdraw expired tx was not handled
    If the user is withdrawing an expired tx the receiver on the event equals the request receiver and the claimid in this case is set to type(uint96).max

--- subgraph/src/utils/constants.ts
+++ subgraph/src/utils/constants.ts
@@ -5,6 +5,8 @@ export const ADDRESS_ZERO_BYTES = Address.fromString(ADDRESS_ZERO_STRING)
 export const ZERO_BI = BigInt.fromI32(0)
 export const ZERO_BYTES = Bytes.fromI32(0)
 
+// solidity's type(uint96).max
+export const CLAIM_ID_EXPIRED_REQUEST = BigInt.fromString("79228162514264337593543950335")
 export const TRANSFER_SIGNATURE = Bytes.fromHexString(
   '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' // This is identifier of the Transfer
 )
