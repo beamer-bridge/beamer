28da7be1b689c8249c47200460fd4b3d4d09b9bd
Gabriel Buragev <gabriel.buragev96@gmail.com>
2023-08-24 23:12:33 +0200

    frontend: update banner text

--- frontend/src/components/layout/Banner.vue
+++ frontend/src/components/layout/Banner.vue
@@ -2,6 +2,6 @@
   <div
     class="text-md flex h-16 w-full flex-col justify-center bg-fire text-center font-semibold text-rosa md:text-xl lg:text-2xl"
   >
-    ✦ Enjoy 0 bridging fees for transfers over $250 from any L2 ($500 from L1). ✦
+    ✦ Enjoy 0 bridging fees for transfers over $250 from any L2. ✦
   </div>
 </template>
